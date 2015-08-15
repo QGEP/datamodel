0b661b150c958043a255f00631cfa726d9d2c006
Matthias Kuhn <matthias@opengis.ch>
2015-08-15 10:16:46 +0200

    Add indexes on re_maintenance_event_wastewater_structure
    Fixes https://github.com/QGEP/QGEP/issues/113

--- 11_n_m_relations.sql
+++ 11_n_m_relations.sql
@@ -1,8 +1,8 @@
 -------
 CREATE TABLE qgep.re_maintenance_event_wastewater_structure
 (
-  fk_wastewater_structure varchar (16),
-  fk_maintenance_event varchar (16),
+  fk_wastewater_structure varchar (16) NOT NULL,
+  fk_maintenance_event varchar (16) NOT NULL,
   CONSTRAINT rel_maintenance_event_wastewater_structure_wastewater_structure FOREIGN KEY (fk_wastewater_structure) REFERENCES qgep.od_wastewater_structure(obj_id),
   CONSTRAINT rel_maintenance_event_wastewater_structure_maintenance_event FOREIGN KEY (fk_maintenance_event) REFERENCES qgep.od_maintenance_event(obj_id),
   CONSTRAINT pkey_qgep_re_maintenance_event_wastewater_structure_obj_id PRIMARY KEY (fk_wastewater_structure, fk_maintenance_event)
@@ -10,3 +10,6 @@ CREATE TABLE qgep.re_maintenance_event_wastewater_structure
 WITH (
    OIDS = False
 );
+
+CREATE INDEX in_re_me_ws__fk_ws ON qgep.re_maintenance_event_wastewater_structure USING btree (fk_wastewater_structure ASC);
+CREATE INDEX in_re_me_ws__fk_me ON qgep.re_maintenance_event_wastewater_structure USING btree (fk_maintenance_event ASC);
