2cbb6a00843122e66df11460c9f0bfa98cdda6b5
SJiB <stefan.burckhardt@gmx.ch>
2015-11-10 15:24:55 +0100

    test_relation - debug details
    identifier instead of obj_id

--- test/test_relations.py
+++ test/test_relations.py
@@ -46,8 +46,10 @@ class TestRelations(unittest.TestCase, DbTestBase):
         cur.execute("DELETE FROM qgep.od_structure_part WHERE obj_id='{obj_id}'".format(obj_id=obj_id))
         
         # count amount of structure part elements and cover elements - should be one each
-        amount_structure_part = cur.execute("SELECT COUNT(*) FROM qgep.od_structure_part WHERE obj_id IN ('CO123', 'CO456')")
-        amount_cover = cur.execute("SELECT COUNT(*) FROM qgep.od_cover WHERE obj_id IN ('CO123', 'CO456')")
+        # 10.11.2015 amount_structure_part = cur.execute("SELECT COUNT(*) FROM qgep.od_structure_part WHERE obj_id IN ('CO123', 'CO456')")
+        amount_structure_part = cur.execute("SELECT COUNT(*) FROM qgep.od_structure_part WHERE identifier IN ('CO123', 'CO456')")
+        # 10.11.2015 amount_cover = cur.execute("SELECT COUNT(*) FROM qgep.od_cover WHERE obj_id IN ('CO123', 'CO456')")
+        amount_cover = cur.execute("SELECT COUNT(*) FROM qgep.od_cover WHERE identifier IN ('CO123', 'CO456')")
         
         print "amount_structure_part = ", amount_structure_part
         print "amount_cover =", amount_cover
