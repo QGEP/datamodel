0a9d34dcc0b5928d159f0cfd39c6fca65d885c64
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-03-31 10:25:15 +0200

    fix typo

--- fixes/fix_vsa_kek_extension.sql
+++ fixes/fix_vsa_kek_extension.sql
@@ -4,7 +4,6 @@
 ALTER TABLE qgep.od_damage_manhole RENAME damage_code_manhole TO damage_code;
 ALTER TABLE qgep.od_damage_channel RENAME channel_damage_code TO damage_code;
 
-
 -- set quantification1/2 type to integer
 ALTER TABLE qgep.od_damage_manhole ALTER COLUMN quantification1 SET DATA TYPE integer USING quantification1::integer;
 ALTER TABLE qgep.od_damage_manhole ALTER COLUMN quantification2 SET DATA TYPE integer USING quantification2::integer;
@@ -15,3 +14,5 @@ ALTER TABLE qgep.od_damage_channel ALTER COLUMN quantification2 SET DATA TYPE in
 ALTER TABLE qgep.vl_damage_channel_channel_damage_code RENAME TO vl_damage_channel_code;
 ALTER TABLE qgep.vl_damage_manhole_damage_code_manhole RENAME TO vl_damage_manhole_code;
 
+--fix typo
+ALTER TABLE qgep.od_examination RENAME inspected_lenght TO inspected_length;
