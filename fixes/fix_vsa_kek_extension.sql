0790613cb739ff116151eccd11da231a2bdf5f1b
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-04-08 09:32:53 +0200

    add missing entries to dictionary table

--- fixes/fix_vsa_kek_extension.sql
+++ fixes/fix_vsa_kek_extension.sql
@@ -1,5 +1,12 @@
 
 
+-- add missing table to dictionary
+INSERT INTO qgep.is_dictionary_od_table (id, tablename, name_en, shortcut_en, name_de, shortcut_de, name_fr, shortcut_fr, name_it, shortcut_it, name_ro, shortcut_ro) VALUES (99990,'od_damage','damage','DM','Beschädigung','BD','Dégâts','DG','zzz','zz','zzz','zz');
+INSERT INTO qgep.is_dictionary_od_table (id, tablename, name_en, shortcut_en, name_de, shortcut_de, name_fr, shortcut_fr, name_it, shortcut_it, name_ro, shortcut_ro) VALUES (99991,'od_examination','examniation','EM','Untersuchung','US','Inspection','IS','zzz','zz','zzz','zz');
+INSERT INTO qgep.is_dictionary_od_table (id, tablename, name_en, shortcut_en, name_de, shortcut_de, name_fr, shortcut_fr, name_it, shortcut_it, name_ro, shortcut_ro) VALUES (99992,'od_damage_channel','channel damage','CD','xxx','xx','Dégâts de collecteur','DC','zzz','zz','zzz','zz');
+INSERT INTO qgep.is_dictionary_od_table (id, tablename, name_en, shortcut_en, name_de, shortcut_de, name_fr, shortcut_fr, name_it, shortcut_it, name_ro, shortcut_ro) VALUES (99993,'od_damage_manhole','manhole damage','MD','xxx','xx','Dégâts de chambre','DH','zzz','zz','zzz','zz');
+
+
 -- rename damage code
 ALTER TABLE qgep.od_damage_manhole RENAME damage_code_manhole TO damage_code;
 ALTER TABLE qgep.od_damage_channel RENAME channel_damage_code TO damage_code;
