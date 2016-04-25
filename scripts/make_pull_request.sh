da9b5812482de6c893c13ac881eca802f5009ea7
Matthias Kuhn <matthias@opengis.ch>
2016-04-25 10:38:22 +0200

    Automatically create pull requests when datamodel is updated
    Whenever datamodel changes are merged to (datamodel) master, a new pull request
    will be opened on QGEP/QGEP to run all the checks there and make it easy to
    merge the datamodel change and have an updated demo dataset.

---
+++ scripts/make_pull_request.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
+then
+  echo "##########################";
+  echo " ... Creating pull request";
+  echo "##########################";
+
+  git fetch origin master
+  printf "Datamodel update from https://github.com/QGEP/QGEP/commit/${TRAVIS_COMMIT}\n\n" > /tmp/commitmessage
+  git log --pretty --no-merges FETCH_HEAD..HEAD >> /tmp/commitmessage
+
+  git clone git@github.com:qgep-bot/QGEP.git
+  pushd QGEP
+  git submodule update --init
+  git checkout -b pullRequest-${TRAVIS_COMMIT}
+  pushd datamodel
+  git checkout ${TRAVIS_COMMIT}
+  popd
+  git add datamodel
+  git commit -F /tmp/commitmessage
+  git push origin pullRequest-${TRAVIS_COMMIT}
+  curl --request POST -H "Authorization: token ${OAUTH_TOKEN}" --data '{ "title": "Datamodel update", "body": "'"`awk 1 ORS='\\\\n' /tmp/commitmessage`"'", "head": "qgep-bot:pullRequest-'"${TRAVIS_COMMIT}"'", "base": "master" }' https://api.github.com/repos/QGEP/QGEP/pulls
+  popd
+fi
