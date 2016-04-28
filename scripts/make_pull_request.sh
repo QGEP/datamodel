d3f4978cd61350ab8218b2bed9d0b0c9b6b903d0
Matthias Kuhn <matthias@opengis.ch>
2016-04-28 11:05:54 +0200

    Make pull requests based on latest QGEP master

--- scripts/make_pull_request.sh
+++ scripts/make_pull_request.sh
@@ -11,7 +11,7 @@ then
   echo " ... Creating pull request";
   echo "##########################";
 
-  git clone git@github.com:qgep-bot/QGEP.git
+  git clone git@github.com:QGEP/QGEP.git
   master=`git -C QGEP submodule status | grep datamodel | cut -d' ' -f1 | cut -d'-' -f2`
   printf "Datamodel update from https://github.com/QGEP/QGEP/commit/${TRAVIS_COMMIT}\n\n" > /tmp/commitmessage
   git log --pretty --no-merges ${master}..HEAD >> /tmp/commitmessage
@@ -24,7 +24,8 @@ then
   popd
   git add datamodel
   git commit -F /tmp/commitmessage
-  git push origin pullRequest-${TRAVIS_COMMIT}
+  git remote add qgep-bot git@github.com:qgep-bot/QGEP.git
+  git push qgep-bot pullRequest-${TRAVIS_COMMIT}
   curl --request POST -H "Authorization: token ${OAUTH_TOKEN}" --data '{ "title": "Datamodel update", "body": "'"`awk 1 ORS='\\\\n' /tmp/commitmessage`"'", "head": "qgep-bot:pullRequest-'"${TRAVIS_COMMIT}"'", "base": "master" }' https://api.github.com/repos/QGEP/QGEP/pulls
   popd
 fi
