baac2795393bd24915e09ac01709dc143256a9fa
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-06-17 07:26:50 +0200

    followup 32aba7c

--- fixes/fix_vsa_kek_extension.sql
+++ fixes/fix_vsa_kek_extension.sql
@@ -1,4 +1,4 @@
 
 
 ALTER TABLE qgep.od_damage ADD COLUMN fk_examination varchar (16);
-ALTER TABLE qgep.od_damage ADD CONSTRAINT rel_damage_examination FOREIGN KEY (fs_examination) REFERENCES qgep.od_examination(obj_id);
+ALTER TABLE qgep.od_damage ADD CONSTRAINT rel_damage_examination FOREIGN KEY (fk_examination) REFERENCES qgep.od_examination(obj_id);
