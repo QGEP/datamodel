32aba7cf3f3fe2e1f59c388e66a62f25ed888951
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-06-17 07:22:35 +0200

    KEK fix: damage - examination relation

--- fixes/fix_vsa_kek_extension.sql
+++ fixes/fix_vsa_kek_extension.sql
@@ -1,19 +1,4 @@
--- rename damage code
-ALTER TABLE qgep.od_damage_manhole RENAME damage_code_manhole TO damage_code;
-ALTER TABLE qgep.od_damage_channel RENAME channel_damage_code TO damage_code;
 
--- set quantification1/2 type to integer
-ALTER TABLE qgep.od_damage_manhole ALTER COLUMN quantification1 SET DATA TYPE integer USING quantification1::integer;
-ALTER TABLE qgep.od_damage_manhole ALTER COLUMN quantification2 SET DATA TYPE integer USING quantification2::integer;
-ALTER TABLE qgep.od_damage_channel ALTER COLUMN quantification1 SET DATA TYPE integer USING quantification1::integer;
-ALTER TABLE qgep.od_damage_channel ALTER COLUMN quantification2 SET DATA TYPE integer USING quantification2::integer;
 
--- rename vl tables
-ALTER TABLE qgep.vl_damage_channel_channel_damage_code RENAME TO vl_damage_channel_code;
-ALTER TABLE qgep.vl_damage_manhole_damage_code_manhole RENAME TO vl_damage_manhole_code;
-
---fix typo
-ALTER TABLE qgep.od_examination RENAME inspected_lenght TO inspected_length;
-
--- remove redundant field
-ALTER TABLE qgep.od_examination DROP COLUMN operator;
+ALTER TABLE qgep.od_damage ADD COLUMN fk_examination varchar (16);
+ALTER TABLE qgep.od_damage ADD CONSTRAINT rel_damage_examination FOREIGN KEY (fs_examination) REFERENCES qgep.od_examination(obj_id);
