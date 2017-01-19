7a2fa116352a767a9c25cd34877b7e0100dd5df2
Matthias Kuhn <matthias@opengis.ch>
2017-01-19 19:49:15 +0100

    Add vw_organisation
    References https://github.com/QGEP/QGEP/issues/266

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -87,3 +87,4 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_wastewater_node
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_qgep_wastewater_structure.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_qgep_reach.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "$(${DIR}/view/vw_oo_overflow.py ${PGSERVICE} ${SRID})"
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "$(${DIR}/view/vw_oo_organisation.py ${PGSERVICE})"
