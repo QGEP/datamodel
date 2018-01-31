f67df8f3e305b5bcf369f8cc9754931aa39b5edb
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-01-31 10:12:36 -0400

    setup roles

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -94,3 +94,5 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "$(${DIR}/view/vw_oo_organisat
 
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/view/vw_catchment_area_connections.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/view/vw_change_points.sql
+
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/12_roles.sql
