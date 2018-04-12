4fe3de1035a16c36c983f54a539e65b7704a04a6
Matthias Kuhn <matthias@opengis.ch>
2018-04-12 10:31:08 +0200

    Add view vw_file
    References https://github.com/QGEP/QGEP/issues/369

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -92,6 +92,7 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_wastewater_node
 # psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_qgep_cover.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/view/vw_qgep_wastewater_structure.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_qgep_reach.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/view/vw_file.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "$(${DIR}/view/vw_oo_overflow.py ${PGSERVICE} ${SRID})"
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "$(${DIR}/view/vw_oo_organisation.py ${PGSERVICE})"
 
--- view/vw_file.sql
+++ view/vw_file.sql
@@ -5,7 +5,7 @@
 -- ******************************************************************************
 
 -- Modification of table od_file --> insert fk_data_media
-
+/*
 ALTER TABLE qgep.od_file
 ADD COLUMN fk_data_media character varying(16);
 -- ******************************************************************************
@@ -13,22 +13,21 @@ ADD COLUMN fk_data_media character varying(16);
 -- ******************************************************************************
 
 -- View: qgep.vw_file
--- DROP VIEW qgep.vw_file;
-
+DROP VIEW qgep.vw_file;
+*/
 CREATE OR REPLACE VIEW qgep.vw_file AS 
- SELECT f.obj_id,
+  SELECT f.obj_id,
     f.identifier,
     f.kind AS file_kind,
     f.object,
     f.class,
     -- dm.path,   
     dm.path::text || f.path_relative::text AS _url,
+    f.fk_dataowner as dataowner,
+    f.fk_provider as provider,
     f.remark
    FROM qgep.od_file f
      LEFT JOIN qgep.od_data_media dm ON dm.obj_id::text = f.fk_data_media::text;
-
-ALTER TABLE qgep.vw_file
-  OWNER TO postgres;
 
 -- ******************************************************************************
 -- 3. FUNCTIONS : 
@@ -40,141 +39,100 @@ ALTER TABLE qgep.vw_file
 CREATE OR REPLACE FUNCTION qgep.vw_file_delete()
   RETURNS trigger AS
 $BODY$
-	BEGIN
-		DELETE FROM qgep.od_file WHERE obj_id = OLD.obj_id;
-		RETURN NULL;
-	END;
-	$BODY$
+  BEGIN
+    DELETE FROM qgep.od_file WHERE obj_id = OLD.obj_id;
+  END; $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION qgep.vw_file_delete()
-  OWNER TO postgres;
 
 
+-- ******************************************************************************
 -- Function: qgep.vw_file_insert()
 -- DROP FUNCTION qgep.vw_file_insert();
 CREATE OR REPLACE FUNCTION qgep.vw_file_insert()
   RETURNS trigger AS
 $BODY$
 
-/* Need to get some variables from both table 
- --> Use the NEW._url to access the od_data_media (already set by admin!) 
- --> postgres will have to compare the NEW._url to the already set od_data_media.path and return the one corresponding.
- -->
-*/
-DECLARE
-	_fk_data_media character varying(16);
-	_path_relative character varying(200);
-	_path character varying(100);
+  BEGIN
 
-BEGIN
+    NEW._url = replace(NEW._url, '\', '/');
 
--- _path
-SELECT dm.path into _path
-FROM qgep.od_data_media dm, qgep.vw_file
-WHERE dm.path like substring(NEW._url, 1, char_length(dm.path)); -- the error might be here
-
--- _path_relative
-SELECT 
-	substring(NEW._url, char_length(_path)+1,char_length(NEW._url))
-	into _path_relative
-FROM qgep.od_data_media dm
-WHERE dm.path IN 
-	(
-	SELECT DISTINCT
-		dm.path
-	FROM qgep.od_data_media dm, qgep.vw_file
-
-	WHERE dm.path like substring(NEW._url, 1, char_length(dm.path))
-	);
-
--- _foreign key
-  SELECT obj_id INTO _fk_data_media
-  FROM qgep.od_data_media dm
-  WHERE dm.path like substring(NEW._url, 1, char_length(dm.path));
-
-  INSERT INTO  qgep.od_file(
-
-      obj_id,
+    INSERT INTO qgep.od_file(
       class,
       identifier,
       kind,
       object,
       path_relative,
-      remark,
-      --last_modification,  to be set once the rest is working
-      --fk_dataowner,
-      --fk_provider,
-      fk_data_media
-      )
-  VALUES
-      (qgep.generate_oid('od_file'::text),
+      fk_dataowner,
+      fk_provider,
+      fk_data_media,
+      remark)
+
+    SELECT
       NEW.class,
       NEW.identifier, 
       NEW.file_kind,
       NEW.object,
-      _path_relative,
-      NEW.remark,
-      _fk_data_media
-      );
-  
+      SUBSTRING(NEW._url, LENGTH("path")+1, LENGTH(NEW._url)), -- path_relative,
+      NEW.dataowner, -- fk_dataowner,
+      NEW.provider, -- fk_provider,
+      obj_id, -- fk_data_media
+      NEW.remark  
+    FROM qgep.od_data_media
+    WHERE "path" = SUBSTRING(NEW._url FROM 1 FOR LENGTH("path"))
+    ORDER BY LENGTH("path") DESC
+    LIMIT 1;
+
+    -- FOUND is a special variable which is always FALSE at the beginning of a PL/pgsql function and will be set by
+    -- e.g. INSERT to TRUE if at least one row is affected.
+    IF NOT FOUND THEN
+      RAISE WARNING 'Could not insert. File not in repository set in od_data_media!';
+    END IF;
+
   RETURN NEW;
 END; $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
-ALTER FUNCTION qgep.vw_file_insert()
-  OWNER TO postgres;
   -- ******************************************************************************
-  
--- Not yet corrected -- waiting for INSERT to work.   
 -- Function: qgep.vw_file_update()
 -- DROP FUNCTION qgep.vw_file_update();
 
 CREATE OR REPLACE FUNCTION qgep.vw_file_update()
   RETURNS trigger AS
 $BODY$
-
-DECLARE
-	_fk_data_media character varying(16);
-	_path_relative character varying(200);
-	_path  character varying(100);
 BEGIN
-SELECT 
-	substring(NEW._url, char_length(dm.path)+1,char_length(NEW._url)),
-	path 
-	into _path_relative, _path
-FROM qgep.od_data_media dm
-WHERE dm.path IN 
-	(
-	SELECT DISTINCT
-		dm.path
-	FROM qgep.od_data_media dm, qgep.vw_file
 
-	WHERE dm.path like substring(NEW._url, 1, char_length(dm.path))
-	);
+NEW._url = replace(NEW._url, '\', '/');
 
   UPDATE  qgep.od_file
     SET 
-      identifier = NEW.identifier, 
-      kind = NEW.file_kind,
-      class = NEW.class,
-      path_relative=_path_relative,
-      --substring(NEW._url, char_length(path)+1,char_length(NEW._url)),
+    class = NEW.class,
+    identifier = NEW.identifier,
+    kind = NEW.file_kind,
+    object = NEW.object,
+    path_relative = SUBSTRING(NEW._url, LENGTH(dm.path)+1, LENGTH(NEW._url)), 
+    fk_dataowner = NEW.dataowner,
+    fk_provider = NEW.provider,
+    fk_data_media = dm.id, 
+    remark = NEW.remark
 
-      remark=NEW.remark;
-      
+FROM (
+  SELECT obj_id as id,
+	path
+	FROM qgep.od_data_media
+	WHERE path = SUBSTRING(NEW._url FROM 1 FOR LENGTH(path))
+	ORDER BY LENGTH(path) DESC
+	LIMIT 1)  dm
 
---  (select(obj_id
--- 			from qgep.od_data_media
--- 			where od_data_media.path=new.path))
-  
+WHERE obj_id = OLD.obj_id;
+
+     
   RETURN NEW;
-END; $BODY$
+END; 
+$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION qgep.vw_file_update()
-  OWNER TO postgres;
 
 
 -- TRIGGERS :
@@ -206,5 +164,3 @@ CREATE TRIGGER vw_file_update
   FOR EACH ROW
   EXECUTE PROCEDURE qgep.vw_file_update();
 -- ******************************************************************************
-
-
