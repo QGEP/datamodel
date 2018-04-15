058fd05cc3f12850ca6b4510ce273c994f8f1375
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-04-15 07:12:09 -0400

    config with geo-ninja

--- .deploy/setup_git.sh
+++ .deploy/setup_git.sh
@@ -2,11 +2,15 @@
 
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
-  echo "Setting up git for qgep-bot"
-  openssl aes-256-cbc -K $encrypted_a9c41dbba15f_key -iv $encrypted_a9c41dbba15f_iv -in qgep_rsa.enc -out ~/.ssh/id_rsa -d
-  chmod 600 ~/.ssh/id_rsa;
-  git config --global user.email "qgep@opengis.ch";
-  git config --global user.name "QGEP";
+  # echo "Setting up git for qgep-bot"
+  # openssl aes-256-cbc -K $encrypted_a9c41dbba15f_key -iv $encrypted_a9c41dbba15f_iv -in qgep_rsa.enc -out ~/.ssh/id_rsa -d
+  # chmod 600 ~/.ssh/id_rsa;
+  # git config --global user.email "qgep@opengis.ch";
+  # git config --global user.name "QGEP";
+  # git config --global push.default simple;
+  echo "Setting up git for bot"
+  git config --global user.email "qgisninja@gmail.com";
+  git config --global user.name "Geo-Ninja";
   git config --global push.default simple;
 else
   echo "Setting up readonly access via https"
