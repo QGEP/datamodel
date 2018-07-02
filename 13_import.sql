336ee596de23948579425c6f7cc0f509f7ca39cf
signedav <david@opengis.ch>
2018-07-02 14:44:21 +0200

    in_quarantine value
    in the qgep_import manhole view

--- 13_import.sql
+++ 13_import.sql
@@ -1,4 +1,58 @@
-﻿-- create mobile view
+﻿-- create quarantine table
+
+DROP TABLE IF EXISTS qgep_import.manhole_quarantine;
+
+CREATE TABLE qgep_import.manhole_quarantine
+(
+  obj_id character varying(16),
+  identifier character varying(20),
+  situation_geometry geometry(Point,2056),
+  co_shape integer,
+  co_diameter smallint,
+  co_material integer,
+  co_positional_accuracy integer,
+  co_level numeric(7,3),
+  _depth numeric(6,3),
+  _channel_usage_current integer,
+  ma_material integer,
+  ma_dimension1 smallint,
+  ma_dimension2 smallint,
+  ws_type text,
+  ma_function integer,
+  ss_function integer,
+  remark character varying(80),
+  wn_bottom_level numeric(7,3),
+  photo1 text,
+  photo2 text,
+  inlet_3_material smallint,
+  inlet_3_dimension_mm numeric(6,3),
+  inlet_3_depth_m numeric(6,3),
+  inlet_4_material smallint,
+  inlet_4_dimension_mm numeric(6,3),
+  inlet_4_depth_m numeric(6,3),
+  inlet_5_material smallint,
+  inlet_5_dimension_mm numeric(6,3),
+  inlet_5_depth_m numeric(6,3),
+  inlet_6_material smallint,
+  inlet_6_dimension_mm numeric(6,3),
+  inlet_6_depth_m numeric(6,3),
+  inlet_7_material smallint,
+  inlet_7_dimension_mm numeric(6,3),
+  inlet_7_depth_m numeric(6,3),
+  outlet_1_material smallint,
+  outlet_1_dimension_mm numeric(6,3),
+  outlet_1_depth_m numeric(6,3),
+  outlet_2_material smallint,
+  outlet_2_dimension_mm numeric(6,3),
+  outlet_2_depth_m numeric(6,3),
+  structure_okay boolean DEFAULT false,
+  inlet_okay boolean DEFAULT false,
+  outlet_okay boolean DEFAULT false,
+  deleted boolean DEFAULT false,
+  quarantine_serial SERIAL PRIMARY KEY
+);
+
+-- create mobile view
 
 CREATE OR REPLACE VIEW qgep_import.vw_manhole AS 
  SELECT DISTINCT ON (ws.obj_id) ws.obj_id,
@@ -41,7 +95,12 @@ CREATE OR REPLACE VIEW qgep_import.vw_manhole AS
     NULL::numeric(6, 3) AS outlet_1_depth_m,
     NULL::smallint AS outlet_2_material,
     NULL::numeric(6, 3) AS outlet_2_dimension_mm,
-    NULL::numeric(6, 3) AS outlet_2_depth_m
+    NULL::numeric(6, 3) AS outlet_2_depth_m,
+    (CASE WHEN EXISTS ( SELECT TRUE FROM qgep_import.manhole_quarantine q WHERE q.obj_id = ws.obj_id )
+    THEN TRUE
+    ELSE FALSE 
+    END) AS in_quarantine
+
    FROM qgep_od.vw_qgep_wastewater_structure ws;
 
 
@@ -150,62 +209,7 @@ CREATE TRIGGER on_mutation_make_insert
   EXECUTE PROCEDURE qgep_import.vw_manhole_insert_into_quarantine();
 
 
--- create quarantine table
-
-DROP TABLE IF EXISTS qgep_import.manhole_quarantine;
-
-CREATE TABLE qgep_import.manhole_quarantine
-(
-  obj_id character varying(16),
-  identifier character varying(20),
-  situation_geometry geometry(Point,2056),
-  co_shape integer,
-  co_diameter smallint,
-  co_material integer,
-  co_positional_accuracy integer,
-  co_level numeric(7,3),
-  _depth numeric(6,3),
-  _channel_usage_current integer,
-  ma_material integer,
-  ma_dimension1 smallint,
-  ma_dimension2 smallint,
-  ws_type text,
-  ma_function integer,
-  ss_function integer,
-  remark character varying(80),
-  wn_bottom_level numeric(7,3),
-  photo1 text,
-  photo2 text,
-  inlet_3_material smallint,
-  inlet_3_dimension_mm numeric(6,3),
-  inlet_3_depth_m numeric(6,3),
-  inlet_4_material smallint,
-  inlet_4_dimension_mm numeric(6,3),
-  inlet_4_depth_m numeric(6,3),
-  inlet_5_material smallint,
-  inlet_5_dimension_mm numeric(6,3),
-  inlet_5_depth_m numeric(6,3),
-  inlet_6_material smallint,
-  inlet_6_dimension_mm numeric(6,3),
-  inlet_6_depth_m numeric(6,3),
-  inlet_7_material smallint,
-  inlet_7_dimension_mm numeric(6,3),
-  inlet_7_depth_m numeric(6,3),
-  outlet_1_material smallint,
-  outlet_1_dimension_mm numeric(6,3),
-  outlet_1_depth_m numeric(6,3),
-  outlet_2_material smallint,
-  outlet_2_dimension_mm numeric(6,3),
-  outlet_2_depth_m numeric(6,3),
-  structure_okay boolean DEFAULT false,
-  inlet_okay boolean DEFAULT false,
-  outlet_okay boolean DEFAULT false,
-  deleted boolean DEFAULT false,
-  quarantine_serial SERIAL PRIMARY KEY
-);
-
-
--- create triggerfunctions and triggers for mobile view
+-- create triggerfunctions and triggers for quarantine table 
 
 CREATE OR REPLACE FUNCTION qgep_import.manhole_quarantine_try_structure_update() RETURNS trigger AS $BODY$
 DECLARE 
