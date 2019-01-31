ee346bd0a8e8091885f6b02be0bfb3e5e7085d4e
signedav <david@opengis.ch>
2019-01-31 15:10:13 +0100

    Use of NaN as not available Z value

--- delta/delta_1.1.2_geometry_altitude.sql
+++ delta/delta_1.1.2_geometry_altitude.sql
@@ -8,7 +8,7 @@ DROP MATERIALIZED VIEW IF EXISTS qgep_od.vw_network_node;
 DROP VIEW IF EXISTS qgep_od.vw_qgep_reach;
 DROP VIEW IF EXISTS qgep_od.vw_change_points;
 
-ALTER TABLE qgep_od.reach_point ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(level,0) ), %(SRID)s);
+ALTER TABLE qgep_od.reach_point ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(level,'NaN') ), %(SRID)s);
 
 
 
@@ -24,9 +24,9 @@ DROP VIEW IF EXISTS qgep_import.vw_manhole;
 DROP VIEW IF EXISTS qgep_od.vw_qgep_wastewater_structure;
 DROP VIEW IF EXISTS qgep_od.vw_wastewater_node;
 
-ALTER TABLE qgep_od.wastewater_node ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(bottom_level,0) ), %(SRID)s);
--- in quarantine we do not add the level to the geometry because this will be done on insert
-ALTER TABLE qgep_import.manhole_quarantine ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_Force3D(situation_geometry);
+ALTER TABLE qgep_od.wastewater_node ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(bottom_level,'NaN') ), %(SRID)s);
+-- in quarantine we do not add the level to the geometry because this will be done on insert on table - so it's always 'NaN'
+ALTER TABLE qgep_import.manhole_quarantine ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), 'NaN' ), %(SRID)s);
 
 
 
@@ -38,7 +38,7 @@ ALTER TABLE qgep_import.manhole_quarantine ALTER COLUMN situation_geometry TYPE 
 -----------------------------------------------
 DROP VIEW IF EXISTS qgep_od.vw_cover;
 
-ALTER TABLE qgep_od.cover ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(level,0) ), %(SRID)s);
+ALTER TABLE qgep_od.cover ALTER COLUMN situation_geometry TYPE geometry('POINTZ', %(SRID)s) USING ST_SetSRID( ST_MakePoint( ST_X(situation_geometry), ST_Y(situation_geometry), COALESCE(level,'NaN') ), %(SRID)s);
 
 
 
