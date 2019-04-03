dc5c530eb8ee4f8fae14327ea98fc719b125246b
git@github.com <timothee.produit@gmail.com>
2019-04-03 14:27:13 +0200

    suppress join on view in vw_outfalls

--- swmm_views/vw_swmm_outfalls.sql
+++ swmm_views/vw_swmm_outfalls.sql
@@ -13,14 +13,9 @@ SELECT
 	st_x(wn.situation_geometry) as X_coordinate,
 	st_y(wn.situation_geometry) as Y_coordinate,
 	dp.identifier as description,
-	concat(dp.remark, ',', wn.remark) as tag,
+	concat(dp.remark, ',', we.remark) as tag,
 	wn.bottom_level as invert_elev,
 	'NO' as tide_gate
 FROM qgep_od.vw_discharge_point as dp
-LEFT JOIN qgep_od.vw_wastewater_node wn on fk_wastewater_structure = dp.obj_id
-
-
------------------------------------
--- Default value for view
------------------------------------
--- ALTER VIEW qgep_od.vw_cover ALTER obj_id SET DEFAULT qgep_sys.generate_oid('qgep_od','cover');
+LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = dp.obj_id
+LEFT JOIN qgep_od.wastewater_networkelement we ON we.obj_id::text = wn.obj_id::text;
