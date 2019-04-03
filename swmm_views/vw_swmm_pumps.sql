70e6c5ea049c66fb376c382e4885ab2cf8e6d1ac
git@github.com <timothee.produit@gmail.com>
2019-04-03 14:15:02 +0200

    change pump view according to SB modification

--- swmm_views/vw_swmm_pumps.sql
+++ swmm_views/vw_swmm_pumps.sql
@@ -14,16 +14,10 @@ CREATE OR REPLACE VIEW qgep_swmm.vw_pumps AS
 
 SELECT 
 	pu.obj_id as name,
-	pu.obj_id as inlet_node, -- inlet is the waternode entering the pump
-	to_wn.obj_id as outlet_node, -- outlet is the waternode at the top of next reach
-	start_level as startup_depth,
-	stop_level as shutoff_depth,
-	ws.identifier as description,
-	ws.remark as tag
-FROM qgep_od.pump as pu
-LEFT JOIN qgep_od.wastewater_structure ws on pu.obj_id = ws.obj_id
-LEFT JOIN qgep_od.wastewater_node wn on pu.obj_id = wn.obj_id
-LEFT JOIN qgep_od.wastewater_networkelement ne on ne.obj_id = wn.obj_id
-LEFT JOIN qgep_od.reach in_reach on in_reach.fk_reach_point_to = ne.obj_id
-LEFT JOIN qgep_od.reach out_reach on out_reach.fk_reach_point_from = ne.obj_id
-LEFT JOIN qgep_od.wastewater_node to_wn on to_wn.obj_id = out_reach.fk_reach_point_to
+	pu.fk_wastewater_node as inlet_node, -- inlet is the waternode entering the pump
+	pu.fk_overflow_to as outlet_node, -- outlet is the waternode at the top of next reach
+	pu.start_level as startup_depth,
+	pu.stop_level as shutoff_depth,
+	pu.identifier as description, -- description of pump
+	pu.remark as tag -- remark of pump
+FROM qgep_od.vw_overflow_pump as pu
