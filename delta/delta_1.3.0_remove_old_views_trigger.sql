118e43177aefc118589d4018545b68b4485ed935
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-22 11:11:30 -0500

    fix delta

--- delta/delta_1.3.0_remove_old_views_trigger.sql
+++ delta/delta_1.3.0_remove_old_views_trigger.sql
@@ -1,11 +1,10 @@
 
-DROP TRIGGER FUNCTION vw_qgep_wastewater_structure_INSERT;
-DROP TRIGGER FUNCTION vw_qgep_wastewater_structure_UPDATE;
-DROP TRIGGER FUNCTION vw_qgep_wastewater_structure_DELETE;
+DROP FUNCTION IF EXISTS vw_qgep_wastewater_structure_INSERT;
+DROP FUNCTION IF EXISTS vw_qgep_wastewater_structure_UPDATE;
+DROP FUNCTION IF EXISTS vw_qgep_wastewater_structure_DELETE;
 
-DROP TRIGGER FUNCTION vw_qgep_reach_INSERT;
-DROP TRIGGER FUNCTION vw_qgep_reach_UPDATE;
-DROP TRIGGER FUNCTION vw_qgep_reach_DELETE;
+DROP FUNCTION IF EXISTS vw_qgep_reach_INSERT;
+DROP FUNCTION IF EXISTS vw_qgep_reach_on_UPDATE;
 
 
 
