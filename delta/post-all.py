dbd7dff1b0e32d4d8e7689ebbc641436bcc9edf8
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-23 08:56:08 -0500

    code layout

--- delta/post-all.py
+++ delta/post-all.py
@@ -2,12 +2,12 @@
 
 # https://stackoverflow.com/a/6098238/1548052
 import os, sys, inspect
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
+folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+if folder not in sys.path:
+    sys.path.insert(0, folder)
+subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "..")))
+if subfolder not in sys.path:
+    sys.path.insert(0, subfolder)
 
 from pum.core.deltapy import DeltaPy
 from view.create_views import create_views
