1b98fc093510c67b4b57b0c485f797f0bc0cb4c1
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-23 08:04:48 -0500

    try

--- delta/post-all.py
+++ delta/post-all.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+import os, sys, inspect
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+# Use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+# Info:
+# cmd_folder = os.path.dirname(os.path.abspath(__file__)) # DO NOT USE __file__ !!!
+# __file__ fails if the script is called in different ways on Windows.
+# __file__ fails if someone does os.chdir() before.
+# sys.argv[0] also fails, because it doesn't not always contains the path.
+
 from pum.core.deltapy import DeltaPy
 from view.create_views import create_views
 
@@ -7,4 +24,4 @@ from view.create_views import create_views
 class CreateViews(DeltaPy):
 
     def run(self):
-        create_views()
+        create_views(SRID=self.variable('SRID'))
