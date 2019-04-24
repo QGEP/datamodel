691bf49d4a0e21369b83efef1536743200fa5ce7
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-23 23:06:00 -0500

    remove views at beginning of post-all.py

--- delta/post-all.py
+++ delta/post-all.py
@@ -9,6 +9,7 @@
 # if subfolder not in sys.path:
 #     sys.path.insert(0, subfolder)
 
+import psycopg2
 from pum.core.deltapy import DeltaPy
 from view.create_views import create_views
 
@@ -16,4 +17,11 @@ from view.create_views import create_views
 class CreateViews(DeltaPy):
 
     def run(self):
+        conn = psycopg2.connect("service={0}".format(self.pg_service))
+        cursor = conn.cursor()
+        sql = open('view/drop_views.sql').read()
+        cursor.execute(sql, self.variables)
+        conn.commit()
+        conn.close()
+
         create_views(srid=self.variable('SRID'), pg_service=self.pg_service)
