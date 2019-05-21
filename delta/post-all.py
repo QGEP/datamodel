5e70607abadef4c270adfaed0e7b09005d513ba5
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-05-21 11:07:48 -0500

    newer PUM will remove variable method (#99)

--- delta/post-all.py
+++ delta/post-all.py
@@ -11,10 +11,10 @@ from view.create_views import create_views
 class CreateViews(DeltaPy):
 
     def run(self):
-        qgep_wastewater_structure_extra = self.variable('qgep_wastewater_structure_extra', None)
-        qgep_reach_extra = self.variable('qgep_reach_extra', None)
+        qgep_wastewater_structure_extra = self.variables.get('qgep_wastewater_structure_extra', None)
+        qgep_reach_extra = self.variables.get('qgep_reach_extra', None)
 
-        create_views(srid=self.variable('SRID'),
+        create_views(srid=self.variables.get('SRID'),
                      pg_service=self.pg_service,
                      qgep_wastewater_structure_extra=qgep_wastewater_structure_extra,
                      qgep_reach_extra=qgep_reach_extra)
