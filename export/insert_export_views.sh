8a3752b270ee8e65119deff8affa10d8ec7695cf
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-06-18 12:00:38 +0200

    use pirogue for export views too
    fixes https://github.com/QGEP/QGEP/issues/486

--- export/insert_export_views.sh
+++ export/insert_export_views.sh
@@ -8,6 +8,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -c "DROP SCHEMA IF EXISTS qgep_export CASCADE;"
 psql -c "CREATE SCHEMA qgep_export;"
 
-
-psql -v ON_ERROR_STOP=1 -c "$(${DIR}/vw_export_wasterwater_structure.py ${PGSERVICE})"
-psql -v ON_ERROR_STOP=1 -c "$(${DIR}/vw_export_reach.py ${PGSERVICE})"
+pirogue simple_joins ${DIR}/vw_export_reach.yaml --pg_service ${PGSERVICE}
+pirogue simple_joins ${DIR}/vw_export_wastewater_structure.yaml --pg_service ${PGSERVICE}
