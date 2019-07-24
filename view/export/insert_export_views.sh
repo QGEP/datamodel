f92c67ad1dae04aeeeeca25b61839eb29970bcf5
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-07-24 11:28:05 +0200

    play it safe with PG services

--- view/export/insert_export_views.sh
+++ view/export/insert_export_views.sh
@@ -5,8 +5,8 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-psql -c "DROP SCHEMA IF EXISTS qgep_export CASCADE;"
-psql -c "CREATE SCHEMA qgep_export;"
+psql "service=${PGSERVICE}" -c "DROP SCHEMA IF EXISTS qgep_export CASCADE;"
+psql "service=${PGSERVICE}" -c "CREATE SCHEMA qgep_export;"
 
 pirogue simple_joins ${DIR}/vw_export_reach.yaml --pg_service ${PGSERVICE}
 pirogue simple_joins ${DIR}/vw_export_wastewater_structure.yaml --pg_service ${PGSERVICE}
