bcbd26b5554d342d39c410ed0c2bb3c925387ee9
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-07-26 10:34:14 +0200

    add data fixes to dump v1.0.0

---
+++ test_data/data_fixes.sql
@@ -0,0 +1,5 @@
+
+
+delete from qgep_od.reach_point where situation_geometry is null;
+delete from qgep_od.wastewater_node where situation_geometry is null;
+delete from qgep_od.cover where situation_geometry is null;
