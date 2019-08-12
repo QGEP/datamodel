1cd1a4df842564b447bc34c617583cfa2f78ad2a
lbartoletti <lbartoletti@users.noreply.github.com>
2019-08-12 10:19:05 +0200

    Update audit SQL file
    Update to same version as QWAT. Most notable change add a replay function for pg-history-viewer

--- 01_audit.sql
+++ 01_audit.sql
@@ -1,4 +1,5 @@
 BEGIN;
+
 --
 -- Audited data. Lots of information is available, it's just a matter of how much
 -- you really want to record. See:
@@ -16,16 +17,6 @@ BEGIN;
 -- you're interested in, into a temporary table where you CREATE any useful
 -- indexes and do your analysis.
 --
--- This needs the hstore extension to be installed.
--- Installation instructions for Ubuntu:
--- 
--- * As root install the postgresql-contrib package
---     apt-get install postgresql-contrib
--- 
--- * Create the hstore extension on this database
---   by manually executing or uncommenting the following line
---     CREATE EXTENSION hstore;
-
 DROP TABLE IF EXISTS qgep_sys.logged_actions;
 
 CREATE TABLE qgep_sys.logged_actions (
@@ -72,7 +63,17 @@ COMMENT ON COLUMN qgep_sys.logged_actions.statement_only IS '''t'' if audit even
 CREATE INDEX logged_actions_relid_idx ON qgep_sys.logged_actions(relid);
 CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON qgep_sys.logged_actions(action_tstamp_stm);
 CREATE INDEX logged_actions_action_idx ON qgep_sys.logged_actions(action);
- 
+
+CREATE TABLE qgep_sys.logged_relations (
+    relation_name text not null,
+    uid_column text not null,
+    PRIMARY KEY (relation_name, uid_column)
+);
+
+COMMENT ON TABLE qgep_sys.logged_relations IS 'Table used to store unique identifier columns for table or views, so that events can be replayed';
+COMMENT ON COLUMN qgep_sys.logged_relations.relation_name IS 'Relation (table or view) name (with schema if needed)';
+COMMENT ON COLUMN qgep_sys.logged_relations.uid_column IS 'Name of a column that is used to uniquely identify a row in the relation';
+
 CREATE OR REPLACE FUNCTION qgep_sys.if_modified_func() RETURNS TRIGGER AS $body$
 DECLARE
     audit_row qgep_sys.logged_actions;
@@ -82,10 +83,11 @@ DECLARE
     h_new hstore;
     excluded_cols text[] = ARRAY[]::text[];
 BEGIN
-    IF TG_WHEN <> 'AFTER' THEN
+
+    IF NOT (TG_WHEN IN ('AFTER' , 'INSTEAD OF')) THEN
         RAISE EXCEPTION 'qgep_sys.if_modified_func() may only run as an AFTER trigger';
     END IF;
- 
+
     audit_row = ROW(
         NEXTVAL('qgep_sys.logged_actions_event_id_seq'), -- event_id
         TG_TABLE_SCHEMA::text,                        -- schema_name
@@ -104,75 +106,91 @@ BEGIN
         NULL, NULL,                                   -- row_data, changed_fields
         'f'                                           -- statement_only
         );
- 
+
     IF NOT TG_ARGV[0]::BOOLEAN IS DISTINCT FROM 'f'::BOOLEAN THEN
         audit_row.client_query = NULL;
+
     END IF;
- 
+
     IF TG_ARGV[1] IS NOT NULL THEN
         excluded_cols = TG_ARGV[1]::text[];
     END IF;
- 
+
     IF (TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW') THEN
-        audit_row.row_data = hstore(OLD.*);
-        audit_row.changed_fields =  (hstore(NEW.*) - audit_row.row_data) - excluded_cols;
+        h_old = hstore(OLD.*) - excluded_cols;
+        audit_row.row_data = h_old;
+        h_new = hstore(NEW.*)- excluded_cols;
+        audit_row.changed_fields =  h_new - h_old;
+
         IF audit_row.changed_fields = hstore('') THEN
             -- All changed fields are ignored. Skip this update.
+            RAISE WARNING '[qgep_sys.if_modified_func] - Trigger detected NULL hstore. ending';
             RETURN NULL;
         END IF;
+  INSERT INTO qgep_sys.logged_actions VALUES (audit_row.*);
+  RETURN NEW;
+        
     ELSIF (TG_OP = 'DELETE' AND TG_LEVEL = 'ROW') THEN
         audit_row.row_data = hstore(OLD.*) - excluded_cols;
+  INSERT INTO qgep_sys.logged_actions VALUES (audit_row.*);
+        RETURN OLD;
+        
     ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW') THEN
         audit_row.row_data = hstore(NEW.*) - excluded_cols;
+  INSERT INTO qgep_sys.logged_actions VALUES (audit_row.*);
+        RETURN NEW;
+
     ELSIF (TG_LEVEL = 'STATEMENT' AND TG_OP IN ('INSERT','UPDATE','DELETE','TRUNCATE')) THEN
         audit_row.statement_only = 't';
+        INSERT INTO qgep_sys.logged_actions VALUES (audit_row.*);
+  RETURN NULL;
+
     ELSE
-        RAISE EXCEPTION '[qgep_sys.if_modified_func] - Trigger func added as trigger for unhandled case: %, %',TG_OP, TG_LEVEL;
-        RETURN NULL;
+        RAISE EXCEPTION USING MESSAGE = '[qgep_sys.if_modified_func] - Trigger func added as trigger for unhandled case: '||TG_OP||', '||TG_LEVEL;
+        RETURN NEW;
     END IF;
-    INSERT INTO qgep_sys.logged_actions VALUES (audit_row.*);
-    RETURN NULL;
+
+
 END;
 $body$
 LANGUAGE plpgsql
 SECURITY DEFINER
-SET search_path = pg_catalog, public
-;
- 
- 
+SET search_path = pg_catalog, public;
+
+
 COMMENT ON FUNCTION qgep_sys.if_modified_func() IS $body$
 Track changes TO a TABLE at the statement AND/OR row level.
- 
+
 Optional parameters TO TRIGGER IN CREATE TRIGGER call:
- 
-param 0: BOOLEAN, whether TO log the query text. DEFAULT 't'.
- 
-param 1: text[], COLUMNS TO IGNORE IN updates. DEFAULT [].
- 
+
+param 0: BOOLEAN, whether TO log the query text. default 't'.
+
+param 1: text[], COLUMNS TO IGNORE IN updates. default [].
+
          Updates TO ignored cols are omitted FROM changed_fields.
- 
+
          Updates WITH only ignored cols changed are NOT inserted
          INTO the audit log.
- 
-         Almost ALL the processing work IS still done FOR updates
-         that ignored. IF you need TO save the LOAD, you need TO USE
-         WHEN clause ON the TRIGGER instead.
- 
-         No warning OR error IS issued IF ignored_cols contains COLUMNS
-         that do NOT exist IN the target TABLE. This lets you specify
-         a standard SET of ignored COLUMNS.
- 
-There IS no parameter TO disable logging of VALUES. ADD this TRIGGER AS
-a 'FOR EACH STATEMENT' rather than 'FOR EACH ROW' TRIGGER IF you do NOT
-want TO log row VALUES.
- 
-Note that the user name logged IS the login role FOR the session. The audit TRIGGER
-cannot obtain the active role because it IS reset BY the SECURITY DEFINER invocation
-of the audit TRIGGER its self.
+
+         Almost ALL the processing work IS still done for updates
+         that ignored. If you need to save the load, you need to use
+         WHEN clause on the trigger instead.
+
+         No warning or error is issued if ignored_cols contains columns
+         that do not exist in the target table. This lets you specify
+         a standard set of ignored columns.
+
+There is no parameter to disable logging of values. Add this trigger as
+a 'FOR EACH STATEMENT' rather than 'FOR EACH ROW' trigger if you do not
+want to log row values.
+
+Note that the user name logged is the login role for the session. The audit trigger
+cannot obtain the active role because it is reset by the SECURITY DEFINER invocation
+of the audit trigger its self.
 $body$;
- 
- 
- 
+
+
+
 CREATE OR REPLACE FUNCTION qgep_sys.audit_table(target_table regclass, audit_rows BOOLEAN, audit_query_text BOOLEAN, ignored_cols text[]) RETURNS void AS $body$
 DECLARE
   stm_targets text = 'INSERT OR UPDATE OR DELETE OR TRUNCATE';
@@ -197,16 +215,25 @@ BEGIN
     END IF;
  
     _q_txt = 'CREATE TRIGGER audit_trigger_stm AFTER ' || stm_targets || ' ON ' ||
-             target_table::text ||
+             target_table ||
              ' FOR EACH STATEMENT EXECUTE PROCEDURE qgep_sys.if_modified_func('||
              quote_literal(audit_query_text) || ');';
     RAISE NOTICE '%',_q_txt;
     EXECUTE _q_txt;
- 
+
+    -- store primary key names
+    insert into qgep_sys.logged_relations (relation_name, uid_column)
+         select target_table, a.attname
+           from pg_index i
+           join pg_attribute a on a.attrelid = i.indrelid
+                              and a.attnum = any(i.indkey)
+          where i.indrelid = target_table::regclass
+            and i.indisprimary
+          on conflict do nothing;
 END;
 $body$
-LANGUAGE 'plpgsql';
- 
+LANGUAGE plpgsql;
+
 COMMENT ON FUNCTION qgep_sys.audit_table(regclass, BOOLEAN, BOOLEAN, text[]) IS $body$
 ADD auditing support TO a TABLE.
  
@@ -225,8 +252,117 @@ $body$ LANGUAGE SQL;
 -- And provide a convenience call wrapper for the simplest case
 -- of row-level logging with no excluded cols and query logging enabled.
 --
-CREATE OR REPLACE FUNCTION qgep_sys.audit_table(target_table regclass) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION qgep_sys.audit_table(target_table regclass) RETURNS void AS $body$
 SELECT qgep_sys.audit_table($1, BOOLEAN 't', BOOLEAN 't');
-$$ LANGUAGE 'sql';
+$body$ LANGUAGE 'sql';
+
+COMMENT ON FUNCTION qgep_sys.audit_table(regclass) IS $body$
+Add auditing support to the given table. Row-level changes will be logged with full client query text. No cols are ignored.
+$body$;
+
+CREATE OR REPLACE FUNCTION qgep_sys.unaudit_table(target_table regclass) RETURNS void AS $body$
+BEGIN
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_row ON ' || target_table::text;
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_stm ON ' || target_table::text;
+END;
+$body$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION qgep_sys.replay_event(pevent_id int) RETURNS void AS $body$
+DECLARE
+  query text;
+BEGIN
+    with
+    event as (
+        select * from qgep_sys.logged_actions where event_id = pevent_id
+    )
+    -- get primary key names
+    , where_pks as (
+        select array_to_string(array_agg(uid_column || '=' || quote_literal(row_data->uid_column)), ' AND ') as where_clause
+          from qgep_sys.logged_relations r
+          join event on relation_name = (schema_name || '.' || table_name)
+    )
+    select into query
+        case
+            when action = 'I' then
+                'INSERT INTO ' || schema_name || '.' || table_name ||
+                ' ('||(select string_agg(key, ',') from each(row_data))||') VALUES ' ||
+                '('||(select string_agg(case when value is null then 'null' else quote_literal(value) end, ',') from each(row_data))||')'
+            when action = 'D' then
+                'DELETE FROM ' || schema_name || '.' || table_name ||
+                ' WHERE ' || where_clause
+            when action = 'U' then
+                'UPDATE ' || schema_name || '.' || table_name ||
+                ' SET ' || (select string_agg(key || '=' || case when value is null then 'null' else quote_literal(value) end, ',') from each(changed_fields)) ||
+                ' WHERE ' || where_clause
+        end
+    from
+        event, where_pks
+    ;
+    
+    execute query;
+END;
+$body$
+LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION qgep_sys.replay_event(int) IS $body$
+Replay a logged event.
+ 
+Arguments:
+   pevent_id:  The event_id of the event in qgep_sys.logged_actions to replay
+$body$;
+
+CREATE OR REPLACE FUNCTION qgep_sys.audit_view(target_view regclass, audit_query_text BOOLEAN, ignored_cols text[], uid_cols text[]) RETURNS void AS $body$
+DECLARE
+  stm_targets text = 'INSERT OR UPDATE OR DELETE';
+  _q_txt text;
+  _ignored_cols_snip text = '';
+
+BEGIN
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_row ON ' || target_view::text;
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_stm ON ' || target_view::text;
+ 
+	IF array_length(ignored_cols,1) > 0 THEN
+	    _ignored_cols_snip = ', ' || quote_literal(ignored_cols);
+	END IF;
+	_q_txt = 'CREATE TRIGGER audit_trigger_row INSTEAD OF INSERT OR UPDATE OR DELETE ON ' || 
+		 target_view::TEXT || 
+		 ' FOR EACH ROW EXECUTE PROCEDURE qgep_sys.if_modified_func(' ||
+		 quote_literal(audit_query_text) || _ignored_cols_snip || ');';
+	RAISE NOTICE '%',_q_txt;
+	EXECUTE _q_txt;
+
+    -- store uid columns if not already present
+  IF (select count(*) from qgep_sys.logged_relations where relation_name = (select target_view)::text AND  uid_column= (select unnest(uid_cols))::text) = 0 THEN
+      insert into qgep_sys.logged_relations (relation_name, uid_column)
+       select target_view, unnest(uid_cols);
+  END IF;    
+
+END;
+$body$
+LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION qgep_sys.audit_view(regclass, BOOLEAN, text[], text[]) IS $body$
+ADD auditing support TO a VIEW.
+ 
+Arguments:
+   target_view:      TABLE name, schema qualified IF NOT ON search_path
+   audit_query_text: Record the text of the client query that triggered the audit event?
+   ignored_cols:     COLUMNS TO exclude FROM UPDATE diffs, IGNORE updates that CHANGE only ignored cols.
+   uid_cols:         MANDATORY COLUMNS to use to uniquely identify a row from the view (in order to replay UPDATE and DELETE)
+
+Example:
+  SELECT qgep_sys.audit_view('qgep_od.vw_element_installation', 'true'::BOOLEAN, '{field_to_ignore}'::text[], '{key_field1, keyfield2}'::text[]) 
+$body$;
+ 
+CREATE OR REPLACE FUNCTION qgep_sys.unaudit_view(target_view regclass) RETURNS void AS $body$
+BEGIN
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_row ON ' || target_view::text;
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_stm ON ' || target_view::text;
+END;
+$body$
+LANGUAGE plpgsql;
+
 
 COMMIT;
