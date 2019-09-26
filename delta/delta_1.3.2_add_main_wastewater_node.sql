9e836d34eeb65873df490ab92bb90ba5118d832a
Matthias Kuhn <matthias@opengis.ch>
2019-09-26 16:28:17 +0200

    Set a default main_wastewater_node

--- delta/delta_1.3.2_add_main_wastewater_node.sql
+++ delta/delta_1.3.2_add_main_wastewater_node.sql
@@ -1,3 +1,9 @@
 
 ALTER TABLE qgep_od.wastewater_structure ADD COLUMN fk_main_wastewater_node varchar(16);
 ALTER TABLE qgep_od.wastewater_structure ADD CONSTRAINT rel_wastewater_structure_cover FOREIGN KEY (fk_main_wastewater_node) REFERENCES qgep_od.cover(obj_id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+UPDATE qgep_od.wastewater_structure ws
+  SET fk_main_wastewater_node = wn.obj_id
+    FROM qgep_od.wastewater_node wn
+    LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.obj_id = wn.obj_id
+    WHERE ws.obj_id = ne.fk_wastewater_structure;
