4052093baf28f034521d3569505c8ae895153f16
Matthias Kuhn <matthias@opengis.ch>
2019-09-27 09:00:08 +0200

    Fix relation in delta

--- delta/delta_1.3.2_add_main_wastewater_node.sql
+++ delta/delta_1.3.2_add_main_wastewater_node.sql
@@ -1,6 +1,6 @@
 
 ALTER TABLE qgep_od.wastewater_structure ADD COLUMN fk_main_wastewater_node varchar(16);
-ALTER TABLE qgep_od.wastewater_structure ADD CONSTRAINT rel_wastewater_structure_cover FOREIGN KEY (fk_main_wastewater_node) REFERENCES qgep_od.cover(obj_id) ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE qgep_od.wastewater_structure ADD CONSTRAINT rel_wastewater_structure_main_wastewater_node FOREIGN KEY (fk_main_wastewater_node) REFERENCES qgep_od.wastewater_node(obj_id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 UPDATE qgep_od.wastewater_structure ws
   SET fk_main_wastewater_node = wn.obj_id
