2c2d67c5973bae5103968414ca43fc63dbdd3e92
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-11-29 15:00:19 +0100

    no deltas for auto views

--- delta/delta_1.3.2_fix_default_values_maintenance_views.sql
+++ delta/delta_1.3.2_fix_default_values_maintenance_views.sql
@@ -1,2 +1,2 @@
-ALTER VIEW qgep_od.vw_qgep_maintenance ALTER obj_id SET DEFAULT qgep_sys.generate_oid('qgep_od','maintenance_event');
-ALTER VIEW qgep_od.vw_qgep_damage ALTER obj_id SET DEFAULT qgep_sys.generate_oid('qgep_od','damage');
+
+-- nothing has to be done, views are automatic
