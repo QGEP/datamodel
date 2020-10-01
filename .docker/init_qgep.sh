558c70a83b13f1e26d5915fde9ae560581f17fcc
olivierdalang <olivier.dalang@gmail.com>
2020-10-01 17:12:05 +0200

    use test-and-upgrade on docker setup too

--- .docker/init_qgep.sh
+++ .docker/init_qgep.sh
@@ -97,13 +97,14 @@ fi
 if [ "$#" == "0" ] || [ "$1" == "build_pum" ]; then
 
   recreate_db "qgep_build_pum"
+  recreate_db "qgep_pum_test"
   echo '----------------------------------------'
-  echo "Building database through pum migrations"
+  echo "Building database through pum migrations (test-and-upgrade)"
 
   pum restore -p qgep_build_pum -x --exclude-schema public --exclude-schema topology -- ./test_data/qgep_demodata_1.0.0.dump
   PGSERVICE=qgep_build_pum psql -v ON_ERROR_STOP=on -f test_data/data_fixes.sql
   pum baseline -p qgep_build_pum -t qgep_sys.pum_info -d ./delta/ -b 1.0.0
-  pum upgrade -p qgep_build_pum -t qgep_sys.pum_info -d ./delta/ -v int SRID 2056
+  yes | pum test-and-upgrade -pp qgep_build_pum -pt qgep_pum_test -pc qgep_build -t qgep_sys.pum_info -f /tmp/dump -d ./delta/ -i constraints views --exclude-schema public -v int SRID 2056
 
   echo "Done ! Database qgep_build_pum can now be used."
   echo '----------------------------------------'
