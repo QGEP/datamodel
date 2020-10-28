de6a3adae273eb4f09d1c1ebc72982a54c6478e0
olivierdalang <olivier.dalang@gmail.com>
2020-10-28 16:57:04 +0100

    [docker] rename qgep_build_pum to qgep_prod
    (to match travis, and ease production of release files)

--- .docker/init_qgep.sh
+++ .docker/init_qgep.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ ! "$#" == "0" ]; then
-  if [ ! "$1" == "wait" ] && [ ! "$1" == "release" ] && [ ! "$1" == "release_struct" ] && [ ! "$1" == "build" ] && [ ! "$1" == "build_pum" ]; then
-    echo "arg must be one of : 'wait' 'release' 'release_struct' 'build' 'build_pum'"
+  if [ ! "$1" == "wait" ] && [ ! "$1" == "release" ] && [ ! "$1" == "release_struct" ] && [ ! "$1" == "build" ] && [ ! "$1" == "prod" ]; then
+    echo "arg must be one of : 'wait' 'release' 'release_struct' 'build' 'prod'"
     exit 1
   fi
 fi
@@ -94,19 +94,19 @@ if [ "$#" == "0" ] || [ "$1" == "build" ]; then
 
 fi
 
-if [ "$#" == "0" ] || [ "$1" == "build_pum" ]; then
+if [ "$#" == "0" ] || [ "$1" == "prod" ]; then
 
-  recreate_db "qgep_build_pum"
+  recreate_db "qgep_prod"
   recreate_db "qgep_pum_test"
   echo '----------------------------------------'
   echo "Building database through pum migrations (test-and-upgrade)"
 
-  pum restore -p qgep_build_pum -x --exclude-schema public --exclude-schema topology -- ./test_data/qgep_demodata_1.0.0.dump
-  PGSERVICE=qgep_build_pum psql -v ON_ERROR_STOP=on -f test_data/data_fixes.sql
-  pum baseline -p qgep_build_pum -t qgep_sys.pum_info -d ./delta/ -b 1.0.0
-  yes | pum test-and-upgrade -pp qgep_build_pum -pt qgep_pum_test -pc qgep_build -t qgep_sys.pum_info -f /tmp/dump -d ./delta/ -i constraints views --exclude-schema public -v int SRID 2056
+  pum restore -p qgep_prod -x --exclude-schema public --exclude-schema topology -- ./test_data/qgep_demodata_1.0.0.dump
+  PGSERVICE=qgep_prod psql -v ON_ERROR_STOP=on -f test_data/data_fixes.sql
+  pum baseline -p qgep_prod -t qgep_sys.pum_info -d ./delta/ -b 1.0.0
+  yes | pum test-and-upgrade -pp qgep_prod -pt qgep_pum_test -pc qgep_build -t qgep_sys.pum_info -f /tmp/dump -d ./delta/ -i constraints views --exclude-schema public -v int SRID 2056
 
-  echo "Done ! Database qgep_build_pum can now be used."
+  echo "Done ! Database qgep_prod can now be used."
   echo '----------------------------------------'
 
 fi
