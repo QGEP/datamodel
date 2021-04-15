38d0ce2950d3521c57a30b3b2aa2f8e1984dc38e
Produit <timothee.produit@gmail.com>
2021-04-15 09:34:56 +0200

    remove typo in rsults views

--- swmm_views/17_vw_swmm_results.sql
+++ swmm_views/17_vw_swmm_results.sql
@@ -6,11 +6,11 @@ md.obj_id AS md_obj_id,
 ms.obj_id AS ms_obj_id, ms.dimension, ms.remark AS swmm_parameter,
 mr.obj_id AS mr_obj_id, mr.measuring_duration, mr.time, mr.value,
 wn.situation_geometry AS geom
-FROM qgep_od.wAStewater_structure ws
-JOIN qgep_od.wAStewater_node wn ON wn.obj_id = ws.fk_main_wAStewater_node
+FROM qgep_od.wastewater_structure ws
+JOIN qgep_od.wastewater_node wn ON wn.obj_id = ws.fk_main_wastewater_node
 LEFT JOIN qgep_od.manhole ma ON ma.obj_id = ws.obj_id
 LEFT JOIN qgep_od.special_structure ss ON ss.obj_id = ws.obj_id
-LEFT JOIN  qgep_od.measuring_point mp  ON mp.fk_wAStewater_structure = ws.obj_id
+LEFT JOIN  qgep_od.measuring_point mp  ON mp.fk_wastewater_structure = ws.obj_id
 LEFT JOIN qgep_od.measuring_device md ON md.fk_measuring_point = mp.obj_id
 LEFT JOIN qgep_od.measurement_series ms ON ms.fk_measuring_point = mp.obj_id
 LEFT JOIN qgep_od.measurement_result mr ON mr.fk_measurement_series = ms.obj_id
@@ -18,7 +18,7 @@ WHERE md.remark = 'SWMM Simulation'
 ORDER BY mp.obj_id, mr.time;
 
 CREATE OR REPLACE VIEW qgep_swmm.link_results AS
-SELECT ne.fk_wAStewater_structure AS ws_obj_id, re.obj_id AS re_obj_id,
+SELECT ne.fk_wastewater_structure AS ws_obj_id, re.obj_id AS re_obj_id,
 re.clear_height, re.material,
 mp.obj_id AS mp_obj_id, mp.remark AS swmm_simulation_name,
 md.obj_id AS md_obj_id,
@@ -26,8 +26,8 @@ ms.obj_id AS ms_obj_id, ms.dimension, ms.remark AS swmm_parameter,
 mr.obj_id AS mr_obj_id, mr.measuring_duration, mr.time, mr.value,
 re.progression_geometry AS geom
 FROM qgep_od.reach re
-JOIN qgep_od.wAStewater_networkelement ne ON re.obj_id= ne.obj_id
-LEFT JOIN qgep_od.measuring_point mp ON  mp.fk_wAStewater_structure = ne.fk_wAStewater_structure
+JOIN qgep_od.wastewater_networkelement ne ON re.obj_id= ne.obj_id
+LEFT JOIN qgep_od.measuring_point mp ON  mp.fk_wastewater_structure = ne.fk_wastewater_structure
 LEFT JOIN qgep_od.measuring_device md ON md.fk_measuring_point = mp.obj_id
 LEFT JOIN qgep_od.measurement_series ms ON ms.fk_measuring_point = mp.obj_id
 LEFT JOIN qgep_od.measurement_result mr ON mr.fk_measurement_series = ms.obj_id
