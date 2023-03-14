b3b3e7c26ab09652602cf8a0acc7dacc567bcf8a
cymed <102035587+cymed@users.noreply.github.com>
2023-03-14 13:12:11 +0100

    Update 05_vw_swmm_dividers.sql

--- swmm_views/05_vw_swmm_dividers.sql
+++ swmm_views/05_vw_swmm_dividers.sql
@@ -15,22 +15,24 @@ SELECT
 	0 as PondedArea,
 	wn.situation_geometry as geom,
 	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
+		WHEN ws_st.vsacode IN (7959, 6529, 6526) THEN 'planned'
 		ELSE 'current'
 	END as state,
 	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
+		WHEN cfhi.vsacode in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
 		ELSE 'secondary'
 	END as hierarchy,
 	wn.obj_id as obj_id
 FROM qgep_od.manhole ma
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ma.obj_id::text
+LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
+LEFT JOIN qgep_vl.channel_function_hierarchic cfhi ON cfhi.code=ws._function_hierarchic
 LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.fk_wastewater_structure::text = ws.obj_id::text
 LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = ne.obj_id
 LEFT JOIN qgep_od.cover co on ws.fk_main_cover = co.obj_id
 LEFT JOIN qgep_vl.manhole_function mf on ma.function = mf.code
-WHERE function = 4798 -- separating_structure
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959)
+WHERE mf.vsacode = 4798 -- separating_structure
+AND ws_st.vsacode IN (6530, 6533, 8493, 6529, 6526, 7959)
 
 UNION ALL
 
@@ -46,19 +48,21 @@ SELECT
 	0 as PondedArea,
 	wn.situation_geometry as geom,
 	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
+		WHEN ws_st.vsacode IN (7959, 6529, 6526) THEN 'planned'
 		ELSE 'current'
 	END as state,
 	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
+		WHEN cfhi.vsacode in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
 		ELSE 'secondary'
 	END as hierarchy,
 	wn.obj_id as obj_id
 FROM qgep_od.special_structure ss
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ss.obj_id::text
+LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
+LEFT JOIN qgep_vl.channel_function_hierarchic cfhi ON cfhi.code=ws._function_hierarchic
 LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.fk_wastewater_structure::text = ws.obj_id::text
 LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = ne.obj_id
 LEFT JOIN qgep_od.cover co on ws.fk_main_cover = co.obj_id
 LEFT JOIN qgep_vl.special_structure_function ssf on ss.function = ssf.code
-WHERE function  = 4799 -- separating_structure
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959);
+WHERE mf.vsacode  = 4799 -- separating_structure
+AND ws_st.vsacode IN (6530, 6533, 8493, 6529, 6526, 7959);
