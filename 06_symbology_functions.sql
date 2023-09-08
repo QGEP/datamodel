5229bd2e787a9f81954781fc4f22b37793cfea13
cymed <102035587+cymed@users.noreply.github.com>
2023-09-08 16:35:04 +0200

    Update 06_symbology_functions.sql

--- 06_symbology_functions.sql
+++ 06_symbology_functions.sql
@@ -306,6 +306,7 @@ VOLATILE;
 --  * obj_id of wastewater structure or NULL to update all
 --------------------------------------------------------
 
+------ 8.9.2023 use reach_point's _label /cymed
 ------ 14.9.2022 index labels by wastewater structure for VSA-DSS compliance /cymed
 ------ 14.9.2022 use idx only when more than one entry /cymed
 ------ 15.8.2018 uk adapted label display only for primary wastwater system
@@ -318,9 +319,11 @@ CREATE OR REPLACE FUNCTION qgep_od.update_wastewater_structure_label(_obj_id tex
   $BODY$
   DECLARE
   myrec record;
-
-BEGIN
-UPDATE qgep_od.wastewater_structure ws
+  BEGIN
+  
+  --Update wastewater structure label
+  -- 2023_05_12: use reach point labels
+  UPDATE qgep_od.wastewater_structure ws
 SET _label = label,
     _cover_label = cover_label,
     _bottom_label = bottom_label,
@@ -332,54 +335,83 @@ SELECT   ws_obj_id,
           CASE WHEN count(co_level)<2 THEN array_to_string(array_agg(E'\nC' || '=' || co_level ORDER BY idx DESC), '', '') ELSE
 		  array_to_string(array_agg(E'\nC' || idx || '=' || co_level ORDER BY idx ASC), '', '') END as cover_label,
           array_to_string(array_agg(E'\nB' || '=' || bottom_level), '', '') as bottom_label,
-          CASE WHEN count(rpi_level)<2 THEN array_to_string(array_agg(E'\nI' || '=' || rpi_level ORDER BY idx DESC), '', '') ELSE
-		  array_to_string(array_agg(E'\nI' || idx || '=' || rpi_level ORDER BY idx ASC), '', '') END as input_label,
-          CASE WHEN count(rpo_level)<2 THEN array_to_string(array_agg(E'\nO' || '=' || rpo_level ORDER BY idx DESC), '', '') ELSE
-		  array_to_string(array_agg(E'\nO' || idx || '=' || rpo_level ORDER BY idx ASC), '', '') END as output_label
-  FROM (
-		  SELECT ws.obj_id AS ws_obj_id, ws.identifier AS ws_identifier, parts.co_level AS co_level, parts.rpi_level AS rpi_level, parts.rpo_level AS rpo_level, parts.obj_id, idx, bottom_level AS bottom_level
+		  array_to_string(array_agg(E'\n'||rpi_label|| '=' || rpi_level ORDER BY rpi_label ASC), '', '')  as input_label,
+		  array_to_string(array_agg(rpo_label|| '=' || rpo_level ORDER BY rpo_label ASC), '', '')  as output_label
+		  FROM (
+		  SELECT ws.obj_id AS ws_obj_id
+		  , ws.identifier AS ws_identifier
+		  , parts.co_level AS co_level
+		  , parts.rpi_level AS rpi_level
+		  , parts.rpo_level AS rpo_level
+		  , parts.rpi_label AS rpi_label
+		  , parts.rpo_label AS rpo_label
+		  , parts.obj_id, idx
+		  , parts.bottom_level AS bottom_level
     FROM qgep_od.wastewater_structure WS
 
-    LEFT JOIN (      
-      SELECT coalesce(round(CO.level, 2)::text, '?') AS co_level, NULL::text AS rpi_level, NULL::text AS rpo_level, SP.fk_wastewater_structure ws, SP.obj_id, row_number() OVER(PARTITION BY SP.fk_wastewater_structure) AS idx, NULL::text AS bottom_level
+    LEFT JOIN (
+	  --cover	
+      SELECT 
+		coalesce(round(CO.level, 2)::text, '?') AS co_level
+		, SP.fk_wastewater_structure ws
+		, SP.obj_id
+		, row_number() OVER(PARTITION BY SP.fk_wastewater_structure) AS idx
+		, NULL::text AS bottom_level
+		, NULL::text AS rpi_level
+		, NULL::text  AS rpo_level
+		, NULL::text as rpi_label
+		, NULL::text  AS rpo_label
       FROM qgep_od.structure_part SP
       RIGHT JOIN qgep_od.cover CO ON CO.obj_id = SP.obj_id
       WHERE _all OR SP.fk_wastewater_structure = _obj_id
-      -- Inputs
-      UNION
-      SELECT NULL AS co_level, coalesce(round(RP.level, 2)::text, '?') AS rpi_level, NULL::text AS rpo_level, NE.fk_wastewater_structure ws, RP.obj_id, row_number() OVER(PARTITION BY NE.fk_wastewater_structure ORDER BY ST_Azimuth(RP.situation_geometry,ST_PointN(RE_to.progression_geometry,-2))/pi()*180 ASC), NULL::text AS bottom_level
-      FROM qgep_od.reach_point RP
-      LEFT JOIN qgep_od.wastewater_networkelement NE ON RP.fk_wastewater_networkelement = NE.obj_id
-      INNER JOIN qgep_od.reach RE_to ON RP.obj_id = RE_to.fk_reach_point_to
-      LEFT JOIN qgep_od.wastewater_networkelement NE_to ON NE_to.obj_id = RE_to.obj_id
-      LEFT JOIN qgep_od.channel CH_to ON NE_to.fk_wastewater_structure = CH_to.obj_id
-      WHERE (_all OR NE.fk_wastewater_structure = _obj_id) and CH_to.function_hierarchic in (5062,5064,5066,5068,5069,5070,5071,5072,5074)  ----label only reaches with function_hierarchic=pwwf.*
-      -- Outputs
-      UNION
-      SELECT NULL AS co_level, NULL::text AS rpi_level, 
-		coalesce(round(RP.level, 2)::text, '?') AS rpo_level, 
-		NE.fk_wastewater_structure ws, RP.obj_id, 
-		row_number() OVER(PARTITION BY NE.fk_wastewater_structure 
-						  ORDER BY array_position(ARRAY[4522,4526,4524,4516,4514,4518,520,4571,5322], ch.usage_current),
-						  ST_Azimuth(RP.situation_geometry,ST_PointN(RE_from.progression_geometry,2))/pi()*180 ASC), 
-		NULL::text AS bottom_level
-      FROM qgep_od.reach_point RP
-      LEFT JOIN qgep_od.wastewater_networkelement NE ON RP.fk_wastewater_networkelement = NE.obj_id
-      INNER JOIN qgep_od.reach RE_from ON RP.obj_id = RE_from.fk_reach_point_from
-	  LEFT JOIN qgep_od.wastewater_networkelement NE_RE ON NE_RE.obj_id::text = RE_from.obj_id::text
-	  LEFT JOIN qgep_od.wastewater_structure ws ON NE_RE.fk_wastewater_structure::text = ws.obj_id::text
-      LEFT JOIN qgep_od.channel ch ON ch.obj_id::text = ws.obj_id::text
-      WHERE CASE WHEN _obj_id IS NULL THEN TRUE ELSE NE.fk_wastewater_structure = _obj_id END
       -- Bottom
       UNION
-      SELECT NULL AS co_level, NULL::text AS rpi_level, NULL::text AS rpo_level, ws1.obj_id ws, NULL, NULL, round(wn.bottom_level, 2)::text AS wn_bottom_level
+      SELECT 
+		NULL AS co_level
+		, ws1.obj_id ws
+		, NULL as obj_id
+		, NULL as idx
+		, round(wn.bottom_level, 2)::text AS wn_bottom_level
+		, NULL::text AS rpi_level
+		, NULL::text  AS rpo_level
+		, NULL::text as rpi_label
+		, NULL::text  AS rpo_label
       FROM qgep_od.wastewater_structure ws1
       LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = ws1.fk_main_wastewater_node
       WHERE _all OR ws1.obj_id = _obj_id
-	)AS parts ON ws = ws.obj_id
-    WHERE _all OR ws.obj_id = _obj_id
-	  ) parts
-  GROUP BY ws_obj_id, COALESCE(ws_identifier, '')
+	  UNION
+	  --input	
+      SELECT 
+		NULL AS co_level
+		, NE.fk_wastewater_structure ws
+		, RP.obj_id
+		,NULL as idx
+		, NULL::text AS bottom_level
+		, coalesce(round(RP.level, 2)::text, '?') AS rpi_level
+		, NULL::text AS rpo_level
+		, rp._label as rpi_label
+		,  NULL::text AS rpo_label
+      FROM qgep_od.reach_point RP
+      LEFT JOIN qgep_od.wastewater_networkelement NE ON RP.fk_wastewater_networkelement = NE.obj_id
+      WHERE (_all OR NE.fk_wastewater_structure = _obj_id) and left(RP._label,1)='I'
+      -- output
+      UNION
+      SELECT  NULL AS co_level
+		, NE.fk_wastewater_structure ws
+		, RP.obj_id
+		, NULL as idx
+		, NULL::text AS bottom_level
+		, NULL::text AS rpi_level
+		, coalesce(round(RP.level, 2)::text, '?') AS rpo_level
+		, NULL::text as rpi_label
+		, rp._label AS rpo_label
+      FROM qgep_od.reach_point RP
+      LEFT JOIN qgep_od.wastewater_networkelement NE ON RP.fk_wastewater_networkelement = NE.obj_id
+      WHERE (_all OR NE.fk_wastewater_structure = _obj_id) and left(RP._label,1)='O'
+	) AS parts ON parts.ws = ws.obj_id
+    WHERE TRUE OR ws.obj_id =_obj_id
+		  ) parts
+		  GROUP BY ws_obj_id, COALESCE(ws_identifier, '')
 ) labeled_ws
 WHERE ws.obj_id = labeled_ws.ws_obj_id;
 
@@ -388,6 +420,78 @@ END
 $BODY$
 LANGUAGE plpgsql
 VOLATILE;
+
+--------------------------------------------------------
+-- UPDATE reach point structure label
+-- Argument:
+--  * obj_id of wastewater structure or NULL to update all
+--------------------------------------------------------
+
+------ 8.9.2023 first draft /cymed
+
+CREATE OR REPLACE FUNCTION qgep_od.update_reach_point_label(_obj_id text, _all boolean default false)
+  RETURNS VOID AS
+  $BODY$
+  DECLARE
+  myrec record;
+  BEGIN
+  -- Updates the reach_point labels of the wastewater_structure 
+  -- _obj_id: obj_id of the associatied wastewater structure
+  -- _all: optional boolean to update all reach points
+  
+  
+ --Update reach_point label
+  UPDATE qgep_od.reach_point rp
+  SET _label = rp_label.new_label
+  FROM (
+  with inp as( SELECT
+    ne.fk_wastewater_structure
+    , rp.obj_id
+    , row_number() OVER(PARTITION BY NE.fk_wastewater_structure 
+					ORDER BY ST_Azimuth(rp.situation_geometry,ST_PointN(re.progression_geometry,-2))/pi()*180 ASC) 
+					as idx
+    , count	(*) OVER(PARTITION BY NE.fk_wastewater_structure ) as max_idx				
+      FROM qgep_od.reach_point rp
+      LEFT JOIN qgep_od.wastewater_networkelement ne ON rp.fk_wastewater_networkelement = ne.obj_id
+      INNER JOIN qgep_od.reach re ON rp.obj_id = re.fk_reach_point_to
+      LEFT JOIN qgep_od.wastewater_networkelement ne_re ON ne_re.obj_id = re.obj_id
+      LEFT JOIN qgep_od.channel ch ON ne_re.fk_wastewater_structure = ch.obj_id
+	  LEFT JOIN qgep_od.wastewater_structure ws ON ne_re.fk_wastewater_structure = ws.obj_id
+	  LEFT JOIN qgep_vl.channel_function_hierarchic vl_ch_fh ON vl_ch_fh.code = ch.function_hierarchic
+	  LEFT JOIN qgep_vl.wastewater_structure_status vl_ws_st ON vl_ws_st.code = ws.status
+	  WHERE left(vl_ch_fh.value_en,4)='pwwf' AND vl_ws_st.value_en ILIKE 'operational%%'), -- double percent for sqlalchemy, otherwise we get the error 'dict is not a sequence'
+  outp as( SELECT
+    ne.fk_wastewater_structure
+    , rp.obj_id
+    , row_number() OVER(PARTITION BY NE.fk_wastewater_structure 
+					ORDER BY ST_Azimuth(rp.situation_geometry,ST_PointN(re.progression_geometry,-2))/pi()*180 ASC) 
+					as idx
+    , count	(*) OVER(PARTITION BY NE.fk_wastewater_structure ) as max_idx				
+      FROM qgep_od.reach_point rp
+      LEFT JOIN qgep_od.wastewater_networkelement ne ON rp.fk_wastewater_networkelement = ne.obj_id
+      INNER JOIN qgep_od.reach re ON rp.obj_id = re.fk_reach_point_from
+      LEFT JOIN qgep_od.wastewater_networkelement ne_re ON ne_re.obj_id = re.obj_id
+      LEFT JOIN qgep_od.channel ch ON ne_re.fk_wastewater_structure = ch.obj_id
+	  LEFT JOIN qgep_od.wastewater_structure ws ON ne_re.fk_wastewater_structure = ws.obj_id
+	  LEFT JOIN qgep_vl.channel_function_hierarchic vl_ch_fh ON vl_ch_fh.code = ch.function_hierarchic
+	  LEFT JOIN qgep_vl.wastewater_structure_status vl_ws_st ON vl_ws_st.code = ws.status
+	  WHERE left(vl_ch_fh.value_en,4)='pwwf' AND vl_ws_st.value_en ILIKE 'operational%%') -- double percent for sqlalchemy, otherwise we get the error 'dict is not a sequence'
+  SELECT 'I'||CASE WHEN max_idx=1 THEN '' ELSE idx::text END as new_label
+  , obj_id
+  FROM inp
+    WHERE (_all AND inp.fk_wastewater_structure IS NOT NULL) OR inp.fk_wastewater_structure= _obj_id
+  UNION
+  SELECT 'O'||CASE WHEN max_idx=1 THEN '' ELSE idx::text END as new_label
+  , obj_id
+  FROM outp
+  WHERE (_all AND outp.fk_wastewater_structure IS NOT NULL) OR outp.fk_wastewater_structure= _obj_id) rp_label
+  WHERE rp_label.obj_id=rp.obj_id;
+END
+
+$BODY$
+LANGUAGE plpgsql
+VOLATILE;
+
 
 --------------------------------------------------
 -- ON COVER CHANGE
@@ -585,6 +689,8 @@ LANGUAGE plpgsql VOLATILE;
 -- ON REACH POINT CHANGE
 --------------------------------------------------
 
+-- 8.9.2023 ADD "EXECUTE qgep_od.update_reach_point_label(_ws_obj_id);" / cymed
+
 CREATE OR REPLACE FUNCTION qgep_od.on_reach_point_update()
   RETURNS trigger AS
 $BODY$
@@ -621,6 +727,7 @@ BEGIN
       LEFT JOIN qgep_od.wastewater_networkelement ne ON ws.obj_id = ne.fk_wastewater_structure
       LEFT JOIN qgep_od.reach_point rp ON ne.obj_id = ne_obj_id;
 
+      EXECUTE qgep_od.update_reach_point_label(_ws_obj_id);
       EXECUTE qgep_od.update_wastewater_structure_label(_ws_obj_id);
       EXECUTE qgep_od.update_depth(_ws_obj_id);
   END LOOP;
