74d04e1eb3542587d0f4210f0c87dbcb50db4b2c
SJiB <stefan.burckhardt@gmx.ch>
2023-10-06 10:24:56 +0200

    migrate_dispatch_copy_data adaption

--- scripts/dispatch_table_migration/migrate_dispatch_copy_data.sql
+++ scripts/dispatch_table_migration/migrate_dispatch_copy_data.sql
@@ -86,8 +86,8 @@ BEGIN
         IF _loop_count = 0 THEN
           -- Missing renames
           _destination_table_name := regexp_replace(_destination_table_name,'^hydraulic_characteristic_data','hydraulic_char_data');
-          _destination_table_name := regexp_replace(_destination_table_name,'^overflow_characteristic_kind_overflow_characteristic','overflow_char_kind_overflow_characteristic');
-          _destination_table_name := regexp_replace(_destination_table_name,'^overflow_characteristic_overflow_characteristic_digital','overflow_char_overflow_characteristic_digital');
+          _destination_table_name := regexp_replace(_destination_table_name,'^overflow_characteristic_kind_overflow_characteristic','overflow_char_kind_overflow_char');
+          _destination_table_name := regexp_replace(_destination_table_name,'^overflow_characteristic_overflow_characteristic_digital','overflow_char_overflow_char_digital');
           _loop_count = 1;
           CONTINUE;
         END IF;
