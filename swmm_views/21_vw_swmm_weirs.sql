b9b0f02ce2896ec384cd0af8690dde89a496c126
SJiB <stefan.burckhardt@gmx.ch>
2023-10-06 13:24:45 +0200

    21_vw_swmm_weirs - Integrate  Allow vl extensions in SWMM views #220
    https://github.com/QGEP/datamodel/pull/220 to not have overlapping pullrequests

--- swmm_views/21_vw_swmm_weirs.sql
+++ swmm_views/21_vw_swmm_weirs.sql
@@ -27,9 +27,9 @@ SELECT
 	NULL as message
 FROM qgep_od.prank_weir pw
 LEFT JOIN qgep_od.overflow of ON pw.obj_id = of.obj_id
-LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_characteristic = oc.obj_id
-LEFT JOIN qgep_vl.overflow_char_overflow_characteristic_digital vl_oc_dig ON oc.overflow_characteristic_digital = vl_oc_dig.code
-LEFT JOIN qgep_vl.overflow_char_kind_overflow_characteristic vl_oc_ki ON oc.kind_overflow_characteristic = vl_oc_ki.code
+LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_char = oc.obj_id
+LEFT JOIN qgep_vl.overflow_char_overflow_char_digital vl_oc_dig ON oc.overflow_char_digital = vl_oc_dig.code
+LEFT JOIN qgep_vl.overflow_char_kind_overflow_char vl_oc_ki ON oc.kind_overflow_char = vl_oc_ki.code
 LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = of.fk_wastewater_node
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.fk_main_wastewater_node = wn.obj_id
 LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
@@ -67,9 +67,9 @@ SELECT
 	concat('Leaping weirs are not supported by SWMM, ', lw.obj_id, 'see: https://swmm5.org/2013/07/19/leaping-weir-example-in-swmm-5-and-infoswmm-alternative/') as message
 FROM qgep_od.leapingweir lw
 LEFT JOIN qgep_od.overflow of ON lw.obj_id = of.obj_id
-LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_characteristic = oc.obj_id
-LEFT JOIN qgep_vl.overflow_char_overflow_characteristic_digital vl_oc_dig ON oc.overflow_characteristic_digital = vl_oc_dig.code
-LEFT JOIN qgep_vl.overflow_char_kind_overflow_characteristic vl_oc_ki ON oc.kind_overflow_characteristic = vl_oc_ki.code
+LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_char = oc.obj_id
+LEFT JOIN qgep_vl.overflow_char_overflow_char_digital vl_oc_dig ON oc.overflow_char_digital = vl_oc_dig.code
+LEFT JOIN qgep_vl.overflow_char_kind_overflow_char vl_oc_ki ON oc.kind_overflow_char = vl_oc_ki.code
 LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = of.fk_wastewater_node
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.fk_main_wastewater_node = wn.obj_id
 LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
