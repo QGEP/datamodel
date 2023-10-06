e1639e0aae7f70fd0d030bd8c55e8a505376d22a
SJiB <stefan.burckhardt@gmx.ch>
2023-10-06 13:24:00 +0200

    19_vw_swmm_outlets - Integrate  Allow vl extensions in SWMM views #220
    https://github.com/QGEP/datamodel/pull/220 to not have overlapping pullrequests

--- swmm_views/19_vw_swmm_outlets.sql
+++ swmm_views/19_vw_swmm_outlets.sql
@@ -20,9 +20,9 @@ SELECT
 	wn.obj_id as obj_id
 FROM qgep_od.prank_weir pw
 LEFT JOIN qgep_od.overflow of ON pw.obj_id = of.obj_id
-LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_characteristic = oc.obj_id
-LEFT JOIN qgep_vl.overflow_char_overflow_characteristic_digital vL_oc_dig ON oc.overflow_characteristic_digital = vL_oc_dig.code
-LEFT JOIN qgep_vl.overflow_char_kind_overflow_characteristic vl_oc_ki ON oc.kind_overflow_characteristic = vl_oc_ki.code
+LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_char = oc.obj_id
+LEFT JOIN qgep_vl.overflow_char_overflow_char_digital vL_oc_dig ON oc.overflow_char_digital = vL_oc_dig.code
+LEFT JOIN qgep_vl.overflow_char_kind_overflow_char vl_oc_ki ON oc.kind_overflow_char = vl_oc_ki.code
 LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = of.fk_wastewater_node
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.fk_main_wastewater_node = wn.obj_id
 LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
