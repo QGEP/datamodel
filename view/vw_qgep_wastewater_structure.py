2f752551179d450ced72d98c17df524d2ea649ad
cymed <102035587+cymed@users.noreply.github.com>
2023-10-10 12:55:09 +0200

    Update vw_qgep_wastewater_structure.py

--- view/vw_qgep_wastewater_structure.py
+++ view/vw_qgep_wastewater_structure.py
@@ -70,11 +70,11 @@ def vw_qgep_wastewater_structure(srid: int,
         , {wn_cols}
         , {ne_cols}
 
-        , ws._label
-        , ws._cover_label
-        , ws._bottom_label
-        , ws._input_label
-        , ws._output_label
+        , labl._label
+        , labl._cover_label
+        , labl._bottom_label
+        , labl._input_label
+        , labl._output_label
         , ws._usage_current AS _channel_usage_current
         , ws._function_hierarchic AS _channel_function_hierarchic
 
@@ -88,6 +88,7 @@ def vw_qgep_wastewater_structure(srid: int,
         LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.obj_id = ws.fk_main_wastewater_node
         LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = ws.fk_main_wastewater_node
         LEFT JOIN qgep_od.channel ch ON ch.obj_id = ws.obj_id
+        LEFT JOIN qgep_od.labels labl ON labl.obj_id = ws.obj_id
         {extra_joins}
         WHERE ch.obj_id IS NULL;
 
