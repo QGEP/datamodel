7489b157d09c31ae6859e87c2dd20aa1500bf95b
cymed <102035587+cymed@users.noreply.github.com>
2023-10-25 14:00:23 +0200

    Define divider based on wastewater node instead of wastewater structrure
    Up to now, every wastewater node within a separating structure was modelled as a divider. Now, wastewater nodes are exported as a divider if they have exactly two outgoing reaches.

--- swmm_views/05_vw_swmm_dividers.sql
+++ swmm_views/05_vw_swmm_dividers.sql
@@ -1,64 +1,45 @@
 --------
 -- View for the swmm module class dividers
 -- Question attribute Diverted Link: Name of link which receives the diverted flow. overflow > fk_overflow_to
-CREATE OR REPLACE VIEW qgep_swmm.vw_dividers AS
-
-SELECT
-	ma.obj_id as Name,
-	coalesce(wn.bottom_level,0)  as Elevation,
-	'???' as diverted_link,
-	'CUTOFF' as Type,
-	0 as CutoffFlow,
-	0 as MaxDepth,
-	0 as InitialDepth,
-	0 as SurchargeDepth,
-	0 as PondedArea,
-	wn.situation_geometry as geom,
-	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
-		ELSE 'current'
-	END as state,
-	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
-		ELSE 'secondary'
-	END as hierarchy,
-	wn.obj_id as obj_id
-FROM qgep_od.manhole ma
-LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ma.obj_id::text
-LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.fk_wastewater_structure::text = ws.obj_id::text
-LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = ne.obj_id
-LEFT JOIN qgep_od.cover co on ws.fk_main_cover = co.obj_id
-LEFT JOIN qgep_vl.manhole_function mf on ma.function = mf.code
-WHERE function = 4798 -- separating_structure
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959)
-
-UNION ALL
-
-SELECT
-	ss.obj_id as Name,
-	coalesce(wn.bottom_level,0) as Elevation,
-	'???' as diverted_link,
-	'CUTOFF' as Type,
-	0 as CutoffFlow,
-	0 as MaxDepth,
-	0 as InitialDepth,
-	0 as SurchargeDepth,
-	0 as PondedArea,
-	wn.situation_geometry as geom,
-	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
-		ELSE 'current'
-	END as state,
-	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
-		ELSE 'secondary'
-	END as hierarchy,
-	wn.obj_id as obj_id
-FROM qgep_od.special_structure ss
-LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ss.obj_id::text
-LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.fk_wastewater_structure::text = ws.obj_id::text
-LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = ne.obj_id
-LEFT JOIN qgep_od.cover co on ws.fk_main_cover = co.obj_id
-LEFT JOIN qgep_vl.special_structure_function ssf on ss.function = ssf.code
-WHERE function  = 4799 -- separating_structure
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959);
+CREATE OR REPLACE VIEW qgep_swmm.vw_dividers
+ AS
+ WITH outs AS (
+         SELECT wn_1.obj_id,
+            count(re.obj_id) AS amount
+           FROM qgep_od.wastewater_node wn_1
+             LEFT JOIN qgep_od.reach_point rp ON rp.fk_wastewater_networkelement::text = wn_1.obj_id::text
+             LEFT JOIN qgep_od.reach re ON re.fk_reach_point_from::text = rp.obj_id::text
+             LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.obj_id::text = re.obj_id::text
+             LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ne.fk_wastewater_structure::text
+	 		 LEFT JOIN qgep_od.channel ch ON ch.obj_id::text = ws.obj_id::text
+             LEFT JOIN qgep_vl.wastewater_structure_status ws_st_1 ON ws.status = ws_st_1.code
+	         LEFT JOIN qgep_vl.channel_function_hydraulic ch_fhyd ON ch.function_hydraulic = ch_fhyd.code
+          WHERE (ws_st_1.vsacode = ANY (ARRAY[6530, 6533, 8493, 6529, 6526, 7959]) 
+				 OR ws_st_1.vsacode IS NULL) 
+	 AND ch_fhyd.vsacode!=23 -- pump pipes do not need a divider beforehand
+          GROUP BY wn_1.obj_id
+        )
+ SELECT wn.obj_id AS name,
+    COALESCE(wn.bottom_level, 0::numeric) AS elevation,
+    '???'::text AS diverted_link,
+    'CUTOFF'::text AS type,
+    0 AS cutoffflow,
+    0 AS maxdepth,
+    0 AS initialdepth,
+    0 AS surchargedepth,
+    0 AS pondedarea,
+    wn.situation_geometry AS geom,
+        CASE
+            WHEN ws_st.vsacode = ANY (ARRAY[7959, 6529, 6526]) THEN 'planned'::text
+            ELSE 'current'::text
+        END AS state,
+        CASE
+            WHEN cfhi.vsacode = ANY (ARRAY[5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074]) THEN 'primary'::text
+            ELSE 'secondary'::text
+        END AS hierarchy,
+    wn.obj_id
+   FROM qgep_od.wastewater_node wn
+     LEFT JOIN outs ON outs.obj_id::text = wn.obj_id::text
+     LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON wn._status = ws_st.code
+     LEFT JOIN qgep_vl.channel_function_hierarchic cfhi ON cfhi.code = wn._function_hierarchic
+  WHERE outs.amount = 2 AND (ws_st.vsacode = ANY (ARRAY[6530, 6533, 8493, 6529, 6526, 7959]));
