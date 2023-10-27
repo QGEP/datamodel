51ece7a7453123a9d6defbb3e97f059cd6553fda
cymed <102035587+cymed@users.noreply.github.com>
2023-10-27 13:14:28 +0200

    include vsacode in storages

--- swmm_views/18_vw_swmm_storages.sql
+++ swmm_views/18_vw_swmm_storages.sql
@@ -27,16 +27,18 @@ SELECT
 	CONCAT_WS(',','special_structure', ssf.value_en) as tag,
 	wn.situation_geometry as geom,
 	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
+		WHEN ws_st.vsacode IN (7959, 6529, 6526) THEN 'planned'
 		ELSE 'current'
 	END as state,
 	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
+		WHEN cfhi.vsacode in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
 		ELSE 'secondary'
 	END as hierarchy,
 	wn.obj_id as obj_id
 FROM qgep_od.special_structure ss
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ss.obj_id::text
+LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
+LEFT JOIN qgep_vl.channel_function_hierarchic cfhi ON cfhi.code=ws._function_hierarchic
 LEFT JOIN qgep_od.wastewater_networkelement ne ON ne.fk_wastewater_structure::text = ws.obj_id::text
 LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = ne.obj_id
 LEFT JOIN qgep_od.hydr_geometry hg on hg.obj_id = wn.fk_hydr_geometry
@@ -46,7 +48,7 @@ LEFT JOIN (
 ) as hr on hr.fk_hydr_geometry =  hg.obj_id
 LEFT JOIN qgep_od.cover co on ws.fk_main_cover = co.obj_id
 LEFT JOIN qgep_vl.special_structure_function ssf on ss.function = ssf.code
-WHERE ss.function = ANY ( ARRAY[ -- must be the same list in vw_swmm_junctions
+WHERE ssf.vsacode = ANY ( ARRAY[ -- must be the same list in vw_swmm_junctions
 6397 --"pit_without_drain"
 -- ,245 --"drop_structure"
 ,6398 --"hydrolizing_tank"
@@ -79,7 +81,7 @@ WHERE ss.function = ANY ( ARRAY[ -- must be the same list in vw_swmm_junctions
 -- ,2745 --"vortex_manhole"
 ]
 )
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959)
+AND ws_st.vsacode IN (6530, 6533, 8493, 6529, 6526, 7959)
 
 UNION ALL
 
@@ -107,16 +109,18 @@ SELECT
 	CONCAT_WS(',','infiltration_installation', iik.value_en) as tag,
 	wn.situation_geometry as geom,
 	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
+		WHEN ws_st.vsacode IN (7959, 6529, 6526) THEN 'planned'
 		ELSE 'current'
 	END as state,
 	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
+		WHEN cfhi.vsacode in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
 		ELSE 'secondary'
 	END as hierarchy,
 	wn.obj_id as obj_id
 FROM qgep_od.infiltration_installation as ii
 LEFT JOIN qgep_od.wastewater_structure ws ON ws.obj_id::text = ii.obj_id::text
+LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
+LEFT JOIN qgep_vl.channel_function_hierarchic cfhi ON cfhi.code=ws._function_hierarchic
 LEFT JOIN qgep_od.wastewater_networkelement we ON we.fk_wastewater_structure::text = ws.obj_id::text
 LEFT JOIN qgep_od.wastewater_node wn on wn.obj_id = we.obj_id
 LEFT JOIN qgep_od.hydr_geometry hg on hg.obj_id = wn.fk_hydr_geometry
@@ -125,7 +129,7 @@ LEFT JOIN (
 	FROM qgep_od.hydr_geom_relation
 ) as hr on hr.fk_hydr_geometry =  hg.obj_id
 LEFT JOIN qgep_vl.infiltration_installation_kind iik on ii.kind = iik.code
-WHERE ii.kind IN (
+WHERE iik.vsacode IN (
 --3282	--"with_soil_passage"
 --3285	--"without_soil_passage"
 --3279	--"surface_infiltration"
@@ -138,45 +142,5 @@ WHERE ii.kind IN (
 --278	--"adsorbing_well"
 --3283	--"infiltration_pipe_sections_gallery"
 )
-AND status IN (6530, 6533, 8493, 6529, 6526, 7959)
-
-UNION ALL
-
--- Storage created for the modelisation of prank weir having a H/Q curve
-SELECT
-	wn.obj_id as Name,
-	coalesce(wn.bottom_level,0) as InvertElev,
-	coalesce((wn.backflow_level-wn.bottom_level),0) as MaxDepth,
-	0 as InitDepth,
-	'FUNCTIONAL' as Shape,
-	'0' as CurveCoefficientOrCurveName, -- curve coefficient if FONCTIONAL curve name if TABULAR
-	0 as CurveExponent, -- if FONCTIONAL
-	0 as CurveConstant, -- if FONCTIONAL
-	0 as SurchargeDepth,
-	0 as Fevap,
-	NULL as Psi,
-	NULL as Ksat, 
-	NULL as IMD,
-	concat_ws(';',
-		ws.identifier::text,
-		concat('Storage created for the prank weir: ', pw.obj_id)
-	) as description,
-	'Prank weir' as tag,
-	wn.situation_geometry as geom,
-	CASE 
-		WHEN status IN (7959, 6529, 6526) THEN 'planned'
-		ELSE 'current'
-	END as state,
-	CASE 
-		WHEN ws._function_hierarchic in (5062, 5064, 5066, 5068, 5069, 5070, 5071, 5072, 5074) THEN 'primary'
-		ELSE 'secondary'
-	END as hierarchy,
-	wn.obj_id as obj_id
-FROM qgep_od.prank_weir pw
-LEFT JOIN qgep_od.overflow of ON pw.obj_id = of.obj_id
-LEFT JOIN qgep_od.overflow_char oc ON of.fk_overflow_characteristic = oc.obj_id
-LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = of.fk_wastewater_node
-LEFT JOIN qgep_od.wastewater_structure ws ON ws.fk_main_wastewater_node = wn.obj_id
-WHERE status IN (6530, 6533, 8493, 6529, 6526, 7959)
-AND oc.overflow_characteristic_digital = 6223  --'yes;
-AND oc.kind_overflow_characteristic = 6220; -- h/q relations (Q/Q relations are not supported by SWMM) 
+AND ws_st.vsacode IN (6530, 6533, 8493, 6529, 6526, 7959)
+;
