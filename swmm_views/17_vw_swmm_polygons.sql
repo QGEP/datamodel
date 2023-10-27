2b91a91f4075e6d12842786beb27a243addfa22e
cymed <102035587+cymed@users.noreply.github.com>
2023-10-27 12:41:33 +0200

    Add orifices and
    add orifices
    add storage node polygon
    Alter list of storage special structures

--- swmm_views/17_vw_swmm_polygons.sql
+++ swmm_views/17_vw_swmm_polygons.sql
@@ -4,6 +4,7 @@
 --------
 CREATE OR REPLACE VIEW qgep_swmm.vw_polygons AS
 
+--Subcatchments
 SELECT
   Subcatchment,
   round(ST_X((dp).geom)::numeric,2) as X_Coord,
@@ -20,5 +21,28 @@ SELECT
       hierarchy,
       obj_id
       FROM qgep_swmm.vw_subcatchments
+    ) as foo
+WHERE (dp).path[2] != nvert
+
+UNION ALL
+--Storage nodes
+SELECT
+  StorageNode,
+  round(ST_X((dp).geom)::numeric,2) as X_Coord,
+  round(ST_Y((dp).geom)::numeric,2) as Y_Coord,
+  state,
+  hierarchy,
+  obj_id
+  FROM (
+    SELECT
+      Name As StorageNode,
+      ST_DumpPoints(detail_geometry_geometry) AS dp,
+      ST_NPoints(detail_geometry_geometry) as nvert,
+      state,
+      hierarchy,
+      st.obj_id
+      FROM qgep_swmm.vw_storages st
+	  LEFT JOIN qgep_od.wastewater_networkelement ne on ne.obj_id=st.obj_id
+	  LEFT JOIN qgep_od.wastewater_structure ws on ws.obj_id=ne.fk_wastewater_structure
     ) as foo
 WHERE (dp).path[2] != nvert;	-- dont select last vertex
--- swmm_views/20_vw_swmm_orifices.sql
+++ swmm_views/20_vw_swmm_orifices.sql
@@ -2,14 +2,23 @@ CREATE OR REPLACE VIEW qgep_swmm.vw_orifices AS
 
 SELECT
 NULL::text as Name,
-NULL::text as FromNode,
+wn.obj_id as FromNode,
 NULL::text as ToNode,
 'SIDE'::text as Type,
+'CIRCULAR'::text as Shape,
 0::text as Offset,
 0.65::text as Qcoeff,
 'NO'::text as Gated,
 0 ::text as CloseTime,
-NULL::text as state,
-NULL::text as hierarchy,
+  CASE WHEN ws_st.vsacode = ANY (ARRAY[7959, 6529, 6526]) THEN 'planned' :: text ELSE 'current' :: text END AS state, 
+  CASE WHEN ch_fh.vsacode = ANY (
+    ARRAY[5062, 5064, 5066, 5068, 5069, 
+    5070, 5071, 5072, 5074]
+  ) THEN 'primary' :: text ELSE 'secondary' :: text END AS hierarchy, 
 NULL::text as obj_id
-LIMIT 0;
+FROM qgep_od.throttle_shut_off_unit tsu
+LEFT JOIN qgep_vl.throttle_shut_off_unit_kind tsu_ki ON tsu_ki.code = tsu.kind
+LEFT JOIN qgep_od.wastewater_node wn ON wn.obj_id = tsu.fk_wastewater_node
+LEFT JOIN qgep_vl.wastewater_structure_status ws_st ON ws_st.code = wn._status 
+LEFT JOIN qgep_vl.channel_function_hierarchic ch_fh ON ch_fh.code = wn._function_hierarchic 
+WHERE tsu_ki.vsacode = ANY(ARRAY[13,134,135,252,2746]);
