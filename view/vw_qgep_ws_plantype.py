1e067104d0d4013e40c8813dd6440cd7b1399418
cymed <102035587+cymed@users.noreply.github.com>
2023-11-10 13:59:09 +0100

    Fix indentation error

--- view/vw_qgep_ws_plantype.py
+++ view/vw_qgep_ws_plantype.py
@@ -553,7 +553,7 @@ if __name__ == "__main__":
     plantype_row_sql= "SELECT * from qgep_vl.wastewater_structure_symbol_plantype"
     cursr.execute(plantype_row_sql)
     rows = cursr.fetchall();
-     cursr.close()
+    cursr.close()
     connctn.close()
     for plantype_row in rows:
         vw_qgep_ws_symbol_plantype(srid=srid, pg_service=pg_service, extra_definition=extra_definition, plantype_row=plantype_row)
